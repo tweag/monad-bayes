842a8440dd50219d9d2b11f72d6c24748cfba6ec
Adam Scibior <ams240@cam.ac.uk>
2015-10-27 15:04:34 +0100

    Fixed Examples to match new type classes

--- src/Examples.hs
+++ src/Examples.hs
@@ -14,14 +14,14 @@ import Sampler
 import SMC
 import MCMC
 
-import Data.Random.Distribution.Beta (Beta(Beta))
+import qualified Data.Random.Distribution.Beta as Beta
 import Data.Random.Distribution.Uniform (Uniform(Uniform))
 import Data.Random.Distribution.Normal (Normal(Normal))
-import Data.Random.Distribution.Bernoulli (Bernoulli(Bernoulli))
+--import Data.Random.Distribution.Bernoulli (Bernoulli(Bernoulli))
 import Data.Random.Distribution.Gamma (Gamma(Gamma))
 import Data.Random.Distribution (pdf)
 
-import Numeric.LinearAlgebra.HMatrix
+--import Numeric.LinearAlgebra.HMatrix
 import Control.Monad (liftM, liftM2, mapM)
 import Data.List (partition, nub)
 import Data.Maybe (mapMaybe)
@@ -41,7 +41,7 @@ exampleHMM = liftM reverse states where
   trans (-1) = categorical $ zip [-1..1] [0.1, 0.4, 0.5]
   trans 0    = categorical $ zip [-1..1] [0.2, 0.6, 0.2]
   trans 1    = categorical $ zip [-1..1] [0.15,0.7,0.15]
-  start   = uniform [[-1], [0], [1]]
+  start   = uniformd [[-1], [0], [1]]
   values = [0.9,0.8,0.7,0,-0.025,5,2,0.1,0,
             0.13,0.45,6,0.2,0.3,-1,-1]
 
@@ -75,12 +75,12 @@ finiteHMM n start trans gen =
 ------------------------------------------------------------
 --Dirichlet Processes
 
-stick :: (Monad d, DiscreteDist d) => [Prob] -> [a] -> d a
+stick :: (Monad d, Bernoulli d) => [Prob] -> [a] -> d a
 -- | Stick-breaking function.
 stick (b:breaks) (a:atoms) =
-    choice b (certainly a) (stick breaks atoms)
+    choice b (return a) (stick breaks atoms)
 
-dp :: (Functor d, Monad d, Sampler d, DiscreteDist d, ContinuousDist d) =>
+dp :: (Functor d, Monad d, Sampler d, Bernoulli d, Beta d) =>
         Double -> d a -> d (d a)
 -- | A Dirichlet Process generates a random probability distribution
 -- using the stick-breaking representation.
@@ -139,19 +139,19 @@ normalizePartition xs = mapMaybe (`lookup` trans) xs where
 ------------------------------------------------------------
 --Coin tossing
 
-coinToss :: (Functor d, Monad d, Conditional d, Sampler d) => Double -> Double -> [Bool] -> d Prob
+coinToss :: (Functor d, Monad d, Conditional d, Beta d) => Double -> Double -> [Bool] -> d Prob
 -- | An infinite sequence of coin tosses.
 -- The prior on the coin weight is 'Beta' alpha beta.
-coinToss alpha beta results =
+coinToss a b results =
     register prior results where
-        prior = fmap prob $ external $ Beta alpha beta
+        prior = fmap prob $ beta a b
         register d [] = d
         register d (r:rs) = register (observe d (bernoulli :: Prob -> Explicit Bool) r) rs
 
-coinExact :: Prob -> Prob -> [Bool] -> Beta Prob
+coinExact :: Prob -> Prob -> [Bool] -> Beta.Beta Prob
 -- | Exact posterior for the 'coinToss' example.
 coinExact alpha beta results =
-    Beta (alpha + positive) (beta + negative) where
+    Beta.Beta (alpha + positive) (beta + negative) where
         (p,n) = partition id results
         positive = fromIntegral $ length p
         negative = fromIntegral $ length n
@@ -159,10 +159,10 @@ coinExact alpha beta results =
 ------------------------------------------------------------
 --Die rolling
 
-die :: (Monad d, DiscreteDist d, Integral n) => n -> d n
+die :: (Monad d, UniformD d, Integral n) => n -> d n
 -- | A distribution over the sums of results of n independent rolls of a fair die
 die 0 = return 0
-die 1 = uniform [1,2,3,4,5,6]
+die 1 = uniformd [1,2,3,4,5,6]
 die n = liftM2 (+) (die 1) (die (n-1))
 
 conditionalDie n = condition (\n -> 1 / fromIntegral n) (die n)
