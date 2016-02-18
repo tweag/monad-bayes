34673f05c9fb06b821591738212622b03d9b1b82
Adam Scibior <adscib@gmail.com>
2016-02-18 14:44:33 +0100

    Preliminary implementation

--- src/Dist.hs
+++ src/Dist.hs
@@ -1,8 +1,6 @@
 {-# LANGUAGE
-  GADTs,
   TupleSections,
-  ScopedTypeVariables,
-  MultiParamTypeClasses,
+  GeneralizedNewtypeDeriving,
   FlexibleInstances
  #-}
 
@@ -12,92 +10,125 @@ import System.Random
 import Control.Applicative (Applicative, pure, (<*>))
 import Control.Arrow (first, second)
 import Control.Monad (liftM, liftM2)
+import Data.Number.LogFloat (LogFloat, fromLogFloat, logFloat)
+import Data.Foldable (toList)
+import qualified Data.Map as Map
+
+import Control.Monad.State.Lazy
+import Control.Monad.List
+import Control.Monad.Trans.Maybe
 
 import Base
-import Explicit hiding (djoin)
-import Sampler (external, StdSampler)
 
--- | A symbolic representation of a probabilistic program which basically remembers all applications of 'return' and '>>='.
--- Formally a free model for a probability monad.
--- Additional constructors are Primitive and Conditional.
-data Dist a where
-    -- One element degenerate distribution.
-    Return      :: a -> Dist a
-    -- Marginalization.
-    Bind        :: Dist b -> (b -> Dist a) -> Dist a
-    -- Primitive that can be sampled from.
-    Primitive   :: (Sampleable d) => d a -> Dist a
-    -- Posterior from likelihood and prior.
-    Conditional :: (a -> Prob) -> Dist a -> Dist a
 
-instance Functor Dist where
-    fmap  = liftM
+newtype Dist a = Dist (StateT LogFloat [] a)
+    deriving (Functor, Applicative, Monad, MonadState LogFloat)
 
-instance Applicative Dist where
-    pure  = return
+instance MonadDist Dist where
+    categorical d = 
+        Dist $ StateT $ \s ->
+            do
+              (x,p) <- toList d
+              return (x, logFloat p * s)
+    normal = error "Dist does not support continuous distributions"
+    gamma  = error "Dist does not support continuous distributions"
+    beta   = error "Dist does not support continuous distributions"
+
+instance MonadBayes Dist where
+    factor w = modify (* w)
+
+enumerate :: Ord a => Dist a -> [(a,Double)]
+enumerate (Dist d) = simplify (runStateT d 1) where
+    simplify = normalize . compact
+    compact = Map.toAscList . Map.fromListWith (+) . map (second fromLogFloat)
+    normalize xs = map (second (/ norm)) xs where
+        norm = sum (map snd xs)
+
+
+newtype Sampler a = Sampler (StdGen -> a)
+    deriving (Functor)
+
+instance Applicative Sampler where
+    pure = return
     (<*>) = liftM2 ($)
 
-instance Monad Dist where
-    return = Return
-    (>>=)  = Bind
+instance Monad Sampler where
+    return = Sampler . const
+    Sampler d >>= f = Sampler $
+                       \g -> let
+                           x          = d g1
+                           Sampler y  = f x
+                           (g1,g2)    = split g
+         in
+           y g2
 
-instance Dirac a Dist where
-    dirac = return
-
-instance Bernoulli Dist where
-    bernoulli p = Primitive (bernoulli p :: StdSampler Bool)
-
-instance UniformD a Dist where
-    uniformd = Primitive . (uniformd :: [a] -> StdSampler a)
-
-instance Categorical a Dist where
-    categorical = Primitive . (categorical :: [(a,Prob)] -> StdSampler a)
-
-instance Normal Dist where
-    normal m s     = Primitive (normal m s :: StdSampler Double)
-
-instance UniformC Dist where
-    uniformc a b = Primitive (uniformc a b :: StdSampler Double)
-
-instance Exponential Dist where
-    exponential l = Primitive (exponential l :: StdSampler Double)
-
-instance Gamma Dist where
-    gamma a b = Primitive (gamma a b :: StdSampler Double)
-
-instance Beta Dist where
-    beta a b = Primitive (beta a b :: StdSampler Double)
+instance MonadDist Sampler where
+    categorical = undefined
+    normal = undefined
+    gamma = undefined
+    beta = undefined
 
 
 
-instance Conditional Dist where
-    condition c d = Conditional c d
+instance MonadDist m => MonadDist (MaybeT m) where
+    categorical = lift . categorical
+    normal m s  = lift (normal m s)
+    gamma a b   = lift (gamma a b)
+    beta a b    = lift (beta a b)
 
-instance Sampler Dist where
-    sampler = Primitive
+instance MonadDist m => MonadBayes (MaybeT m) where
+    factor = undefined
 
-instance Bayesian Dist where
-    prior (Conditional c d) = do
-        (x,s) <- prior d
-        return (x, s * c x)
-    --Prior is only extracted from the outer distribution.
-    prior (Bind d f) = do
-        (x,p) <- prior d
-        y     <- f x
-        return (y,p)
-    -- Non-recursive cases are not conditional, so they just get score 1.
-    prior d = fmap (,1) d
-
-    prior' (Conditional c d) = prior' d
-    prior' (Bind d f)        = prior' d >>= f
-    prior' d = d
+rejection :: MaybeT Sampler a -> Sampler a
+rejection d = do
+  m <- runMaybeT d
+  case m of Just x  -> return x
+            Nothing -> rejection d
 
 
-instance Sampleable Dist where
-    sample g (Return x)     = x
-    sample g (Primitive d)     = sample g d
-    sample g (Bind d f)        = sample g1 $ f $ sample g2 d where
-        (g1, g2) = split g
-    sample g (Conditional c d) = error "Attempted to sample from a conditional distribution."
+
+instance MonadDist m => MonadDist (StateT s m) where
+    categorical = lift . categorical
+    normal m s  = lift (normal m s)
+    gamma a b   = lift (gamma a b)
+    beta a b    = lift (beta a b)
+
+instance MonadDist m => MonadBayes (StateT LogFloat m) where
+    factor w = modify (* w)
+
+importance :: StateT LogFloat Sampler a -> Sampler (a,LogFloat)
+importance d = runStateT d 1
 
 
+
+
+
+data ParticleT m a = Finished (m a) | Running (m (ParticleT m a))
+
+synchronize :: Monad m => ParticleT m ()
+synchronize = Running (return (return ()))
+
+instance Functor m => Functor (ParticleT m) where
+    fmap f (Finished d) = Finished (fmap f d)
+    fmap f (Running d)  = Running (fmap (fmap f) d)
+
+instance Monad m => Applicative (ParticleT m) where
+    pure = return
+    (<*>) = liftM2 ($)
+
+instance Monad m => Monad (ParticleT m) where
+    return x = Finished (return x)
+    Finished d >>= f = Running (fmap f d)
+    Running  d >>= f = Running (fmap (>>= f) d)
+
+instance MonadTrans ParticleT where
+    lift = Finished
+
+instance MonadDist m => MonadDist (ParticleT m) where
+    categorical = lift . categorical
+    normal m s  = lift (normal m s)
+    gamma a b   = lift (gamma a b)
+    beta a b    = lift (beta a b)
+
+instance MonadBayes m => MonadBayes (ParticleT m) where
+    factor w = lift (factor w) >> synchronize
