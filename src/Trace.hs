36bb0cf9baa6bfd0ddce5151eabebcf49632cfe8
Yufei Cai <yufei.cai@uni-tuebingen.de>
2016-02-22 22:04:59 +0100

    make RandomDB independent from result of execution

--- src/Trace.hs
+++ src/Trace.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
-  GADTs
+  GADTs,
+  DeriveFunctor
  #-}
 
 
@@ -7,101 +8,151 @@ module Trace where
 
 import Control.Monad (liftM,liftM2)
 import Data.Number.LogFloat
+import Text.Printf
 
 import Primitive
 import Base
 import Sampler
 
 -- | A random database of stochastic choices made in the program.
-data RandomDB a where
-    Pure :: a -> RandomDB a
-    Node :: Primitive a -> a -> RandomDB a
-    -- The arguments to Bind are: trace to this point, trace from this point,
-    -- continuation that can be used to generate new trace if the first part changes.
-    Bind :: RandomDB b -> RandomDB a -> (b -> Trace a) -> RandomDB a 
+data RandomDB where
+    None :: RandomDB
+    Node :: Primitive a -> a -> RandomDB
+    -- The arguments to Bind are: trace to this point, trace from this point.
+    Bind :: RandomDB -> RandomDB -> RandomDB
 
--- The three instances below are probably useless.
-instance Functor RandomDB where
-    fmap f (Pure x) = Pure (f x)
-    fmap f d = liftM f d
+-- print the stochastic choices for debugging
+instance Show RandomDB where
+  show None = "None"
+  show (Node (Normal m s) x) = printf "Node (Normal %f %f) %f" m s x
+  show (Node (Gamma  a b) x) = printf "Node (Gamma %f %f) %f" a b x
+  show (Node (Beta   a b) x) = printf "Node (Beta %f %f) %f" a b x
 
-instance Applicative RandomDB where
-    pure = Pure
-    (<*>) = liftM2 ($)
+-- consider saving the score for MonadBayes:
+--   TraceM a = TraceM RandomDB a LogFloat
+data TraceM a = TraceM RandomDB a
+    deriving (Functor)
 
-instance Monad RandomDB where
-    return = pure
-    d >>= f = Bind d (f (value d)) (Trace . return . f)
+-- the monad instance below is used in `instance Applicative Trace` by `liftM2`
+instance Applicative TraceM where
+    pure = TraceM None
+    (TraceM fTrace f) <*> (TraceM xTrace x) =
+       TraceM (Bind fTrace xTrace) (f x)
+
+instance Monad TraceM where
+    (TraceM xTrace x) >>= f =
+        let
+          TraceM yTrace y = f x
+        in
+          TraceM (Bind xTrace yTrace) y
 
 -- | The final value produced by the program.
-value :: RandomDB a -> a
-value (Pure x) = x
-value (Node d x) = x
-value (Bind t1 t2 k) = value t2
+value :: TraceM a -> a
+value (TraceM db x) = x
 
 -- | The number of random choices in the RandomDB.
-size :: RandomDB a -> Int
-size (Pure _) = 0
+size :: RandomDB -> Int
+size None = 0
 size (Node _ _) = 1
-size (Bind t1 t2 _) = size t1 + size t2
+size (Bind t1 t2) = size t1 + size t2
 
 -- | The product of all densities in the trace.
-weight :: RandomDB a -> LogFloat
-weight (Pure _) = 1
+weight :: RandomDB -> LogFloat
+weight None = 1
 weight (Node d x) = pdf d x
-weight (Bind t1 t2 _) = weight t1 * weight t2
+weight (Bind t1 t2) = weight t1 * weight t2
 
 -- | Updates a trace by resampling a randomly selected site.
-update :: RandomDB a -> Sampler (RandomDB a)
-update (Pure x) = return $ Pure x
+update :: RandomDB -> Sampler RandomDB
+update None = return None
 update (Node d x) = fmap (Node d) (primitive d)
-update (Bind t1 t2 k) = do
+update (Bind t1 t2) = do
   let p = 0.5
   b <- bernoulli p
   if b then 
       do
         t1' <- update t1
-        t2' <- runTrace $ k $ value t1'
-        return $ Bind t1' t2' k
+        return $ Bind t1' t2
   else 
       do
         t2' <- update t2
-        return $ Bind t1 t2' k
+        return $ Bind t1 t2'
 
--- | Reuses the random choices from the first computation in the second one.
-reuse :: RandomDB a -> RandomDB a -> Trace a
-reuse = undefined
+-- | Split a @RandomDB@ into two if there is sufficient information
+splitDB :: RandomDB -> (RandomDB, RandomDB)
+splitDB (Bind t1 t2) = (t1, t2)
+splitDB _ = (None, None)
 
-newtype Trace a = Trace (Sampler (RandomDB a))
+-- | A traced program takes the stochastic choices of the previous execution
+-- and generates a new value together with the stochastic choices leading up to it.
+newtype Trace a = Trace (RandomDB -> Sampler (TraceM a))
 runTrace (Trace d) = d
+-- is there any stylistic reason for this instead of `newtype Trace a = Trace { runTrace :: ... }`?
 
 instance Functor Trace where
-    fmap = liftM
+    -- modify final result, leaving stochastic choices intact
+    fmap f (Trace p) = Trace $ fmap (fmap f) . p
 
 instance Applicative Trace where
     pure = return
     (<*>) = liftM2 ($)
 
+-- | Execute program, reusing previous trace
 instance Monad Trace where
-    return = Trace . return . return
-    t >>= f = Trace $
+    return = Trace . const . return . return
+
+    -- | >>= handles reuse of @Bind@ nodes
+    t >>= f = Trace $ \db ->
         do
-          let d = runTrace t
-          r1 <- d
-          let x = value r1
-          r2 <- runTrace (f x)
-          return $ Bind r1 r2 f where
+          let (db1, db2) = splitDB db
+          r1 <- runTrace t db1
+          r2 <- runTrace (f $ value r1) db2
+          return $ r1 >> r2
 
 instance MonadDist Trace where
-    primitive d = Trace $ do
+    -- | @primitive@ handles reuse of @Node@ nodes
+    primitive d = Trace $ \db ->
+        do
           x <- primitive d
-          return $ Node d x
+          let newSample = TraceM (Node d x) x
+          case db of Node d' x -> return $ reusePrimitive d' x d newSample
+                     otherwise -> return newSample
+
     categorical = error "Can not use Trace on categorical without equality"
 
+-- | Reuse previous sample of primitive RV only if type and parameters match exactly.
+reusePrimitive :: Primitive old -> old -> Primitive new -> TraceM new -> TraceM new
+reusePrimitive d@(Normal m s) old (Normal m' s') new | m == m' && s == s' = TraceM (Node d old) old
+reusePrimitive d@(Gamma  a b) old (Gamma  a' b') new | a == a' && b == b' = TraceM (Node d old) old
+reusePrimitive d@(Beta   a b) old (Beta   a' b') new | a == a' && b == b' = TraceM (Node d old) old
+reusePrimitive _ _ _ new = new
+-- Consider re-weighing instead, if weight of previous sample is high enough according to new distribution.
+-- Problem: How can we let the user configure the weight threshold for reuse?
 
-mhStep :: RandomDB a -> Sampler (RandomDB a)
-mhStep r = do
-  r' <- update r
+-- | @mhStep t@ corresponds to the transition kernel of Metropolis-Hastings algorithm
+mhStep :: Trace a -> TraceM a -> Sampler (TraceM a)
+mhStep (Trace p) (TraceM r x) = do
+  -- Single-site mutation of previous stochastic choices.
+  r1 <- update r
+
+  -- Re-execute program with new stochastic choices, reusing as much as possible.
+  -- If program's 'latent space' is fixed and independent from sampling,
+  -- then everything will be reused. After some improvement of @reusePrimitive@,
+  -- everything should be reused in the examples of the `joint` branch.
+  --
+  TraceM r' x' <- p r1
+
+  -- Compute some provisional acceptance ratio
   let ratio = weight r' * fromIntegral (size r') / (weight r * fromIntegral (size r))
+
   accept <- bernoulli ratio
-  return $ if accept then r' else r
+  return $ if accept then TraceM r' x' else TraceM r x
+
+-- TODO
+--
+-- 1. Try it out on an example to gain some confidence.
+--
+-- 2. Support `instance MonadBayes Trace`, since examples of Metropolis-Hastings involve
+--    heavy uses of conditioning.
+--
+-- 3. Make `reusePrimitive` reuse more.
