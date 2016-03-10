8ceb9138f92728d65949f6738039ff9c326c1323
Yufei Cai <yufei.cai@uni-tuebingen.de>
2016-03-10 11:58:59 +0100

    abstract over keytype in multimap as RandomDB

--- src/Trace/ByDist.hs
+++ src/Trace/ByDist.hs
@@ -17,7 +17,7 @@ import Data.Typeable
 import Base
 import Primitive
 import Trace
-
+import Trace.WithMultimap
 import Trace.ByTime hiding (empty)
 
 newtype ByDist = ByDist { runByDist :: [(Prim, Cache)] }
@@ -26,8 +26,20 @@ newtype ByDist = ByDist { runByDist :: [(Prim, Cache)] }
 empty :: ByDist
 empty = ByDist []
 
+-- Boilerplate. Cannot derive this instance due to nominal roles.
+instance RandomDB ByDist where
+  singleton = ByDist . singleton
+  consult d = fmap (second ByDist) . consult d . runByDist
+  mutate = fmap ByDist . mutate . runByDist
+  size = size . runByDist
+  weight = weight . runByDist
+  resampled (ByDist xs) (ByDist ys) = ByDist $ resampled xs ys
+
 data Prim where
   Prim :: Primitive a -> Prim
+
+instance MultimapKey Prim where
+  mkKey = Prim
 
 instance Eq Prim where
   Prim (Categorical xs) == Prim (Categorical ys) = maybe False (== ys) (cast xs)
@@ -35,38 +47,3 @@ instance Eq Prim where
   Prim (Gamma  a b    ) == Prim (Gamma  a' b'  ) = a == a' && b == b'
   Prim (Beta   a b    ) == Prim (Beta   a' b'  ) = a == a' && b == b'
   Prim (_             ) == Prim (_             ) = False
-
-partitionAt :: (Eq a) => a -> [(a, b)] -> ([(a, b)], [(a, b)])
-partitionAt key = partition ((== key) . fst)
-
-instance RandomDB ByDist where
-  record d x = ByDist [(Prim d, Cache d x)]
-
-  consult d (ByDist xs) =
-    let
-      (matched, notMatched) = partitionAt (Prim d) xs
-    in
-      if null matched then
-        Nothing
-      else
-        Just (snd $ head $ matched, ByDist $ tail matched ++ notMatched)
-
-  size (ByDist xs) = length xs
-
-  weight (ByDist xs) = weight $ map snd xs
-
-  resampled (ByDist xs) (ByDist ys) =
-    ByDist $ loop (nub (map fst ys)) xs ys
-    where
-      loop [] xs ys = []
-      loop (key : keys) xs ys =
-        let
-          (kx, xs') = partitionAt key xs
-          (ky, ys') = partitionAt key ys
-        in
-          (map ((,) key) $ resampled (map snd kx) (map snd ky)) ++ loop keys xs' ys'
-
-  mutate (ByDist xs) = do
-    let keys = map fst xs
-    caches' <- mutate $ map snd xs
-    return $ ByDist $ zip keys caches'
