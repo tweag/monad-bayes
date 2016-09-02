77df71e4fa6d3713dafecb68d204a8c64d880d52
Adam Scibior <adscib@gmail.com>
2016-09-02 18:21:51 +0200

    Clean up Weighted

--- src/Control/Monad/Bayes/Weighted.hs
+++ src/Control/Monad/Bayes/Weighted.hs
@@ -40,22 +40,9 @@ unWeight (Weight (Product p)) = p
 -- | A wrapper for 'WriterT' ('Weight' 'r') that executes the program
 -- emitting the likelihood score.
 newtype Weighted m a = Weighted {toStateT :: StateT (Weight (CustomReal m)) m a}
-    deriving(Functor)
-
-deriving instance MonadDist m => Applicative (Weighted m)
-deriving instance MonadDist m => Monad (Weighted m)
+    deriving(Functor, Applicative, Monad)
 
 type instance CustomReal (Weighted m) = CustomReal m
-
-runWeighted :: MonadDist m => Weighted m a -> m (a, LogDomain (CustomReal m))
-runWeighted = fmap (second unWeight) . (`runStateT` 1) . toStateT
-
-withWeight :: MonadDist m => m (a, LogDomain (CustomReal m)) -> Weighted m a
-withWeight m = Weighted $ do
-  (x,w) <- lift m
-  put $ weight w
-  return x
-
 
 instance MonadTrans Weighted where
   lift = Weighted . lift
@@ -66,25 +53,27 @@ instance MonadDist m => MonadDist (Weighted m) where
 instance MonadDist m => MonadBayes (Weighted m) where
   factor w = Weighted $ modify (* weight w)
 
+runWeighted :: MonadDist m => Weighted m a -> m (a, LogDomain (CustomReal m))
+runWeighted = fmap (second unWeight) . (`runStateT` 1) . toStateT
+
+withWeight :: MonadDist m => m (a, LogDomain (CustomReal m)) -> Weighted m a
+withWeight m = Weighted $ do
+  (x,w) <- lift m
+  put $ weight w
+  return x
+
 -- | Similar to 'Weighted', only the weight is both recorded and passed
 -- to the underlying monad. Useful for getting the exact posterior and
 -- the associated likelihood.
 newtype WeightRecorderT m a =
   WeightRecorderT {runWeightRecorderT :: Weighted m a}
-    deriving(Functor)--, Applicative, Monad, MonadTrans, MonadDist)
-deriving instance MonadDist m => Applicative (WeightRecorderT m)
-deriving instance MonadDist m => Monad (WeightRecorderT m)
+    deriving(Functor, Applicative, Monad, MonadTrans)
 type instance CustomReal (WeightRecorderT m) = CustomReal m
+deriving instance MonadDist m => MonadDist (WeightRecorderT m)
+
+instance MonadBayes m => MonadBayes (WeightRecorderT m) where
+  factor w = WeightRecorderT (factor w >> lift (factor w))
 
 -- | Both record weight and pass it to the underlying monad.
 duplicateWeight :: MonadBayes m => WeightRecorderT m a -> Weighted m a
 duplicateWeight = runWeightRecorderT
-
-instance MonadTrans (WeightRecorderT) where
-  lift = WeightRecorderT . lift
-
-instance MonadDist m => MonadDist (WeightRecorderT m) where
-  primitive = WeightRecorderT . primitive
-
-instance MonadBayes m => MonadBayes (WeightRecorderT m) where
-  factor w = WeightRecorderT (factor w >> lift (factor w))
