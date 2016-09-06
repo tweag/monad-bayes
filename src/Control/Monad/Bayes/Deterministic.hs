d1ad591bd248755eb21309520b07e0cd843a419f
Adam Scibior <adscib@gmail.com>
2016-09-06 13:43:42 +0200

    Make the MonadBayes instance of Deterministic polymorphic

--- src/Control/Monad/Bayes/Deterministic.hs
+++ src/Control/Monad/Bayes/Deterministic.hs
@@ -9,6 +9,8 @@ module Control.Monad.Bayes.Deterministic(
   maybeDeterministic
 ) where
 
+import Data.Typeable
+
 import Control.Monad.Bayes.LogDomain (LogDomain, NumSpec)
 import Control.Monad.Bayes.Class
 
@@ -22,10 +24,10 @@ newtype Deterministic r a = Deterministic (Maybe a)
 
 type instance CustomReal (Deterministic r) = r
 
-instance MonadDist (Deterministic Double) where
+instance (Ord r, Typeable r, Real r, NumSpec r) => MonadDist (Deterministic r) where
   primitive d = Deterministic Nothing
 
-instance MonadBayes (Deterministic Double) where
+instance (Ord r, Typeable r, Real r, NumSpec r) => MonadBayes (Deterministic r) where
   factor w = Deterministic Nothing
 
 -- | Converts a probabilistic type into a deterministic one,
