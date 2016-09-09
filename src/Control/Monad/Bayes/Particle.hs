fc07cd0c5daab20d1f904e5ca1021f1fddd23701
Adam Scibior <adscib@gmail.com>
2016-09-09 14:32:18 +0200

    Wrap Particle in a newtype

--- src/Control/Monad/Bayes/Particle.hs
+++ src/Control/Monad/Bayes/Particle.hs
@@ -29,33 +29,34 @@ import Control.Monad.Bayes.Class
 -- for implementation of SMC-related methods.
 -- All the probabilistic effects are lifted from the transformed monad,
 -- but also `synchronize` is inserted after each `factor`.
-type Particle = Coroutine (Await ())
+newtype Particle m a = Particle {runParticle :: (Coroutine (Await ()) m a)}
+  deriving(Functor,Applicative,Monad,MonadTrans)
 extract (Await f) = f ()
 
 -- | A synchronization barrier where computation is paused.
 synchronize :: Monad m => Particle m ()
-synchronize = await
+synchronize = Particle await
 
 -- | Remove the synchronization barriers.
 flatten :: Monad m => Particle m a -> m a
-flatten = pogoStick extract
+flatten = pogoStick extract . runParticle
 
 -- | Run a particle to the next barrier.
 -- If the computation is finished do nothing.
 --
 -- > flatten = flatten . advance
 advance :: Monad m => Particle m a -> Particle m a
-advance = bounce extract
+advance = Particle . bounce extract . runParticle
 
 -- | Checks if the particle is finished.
 finished :: Monad m => Particle m a -> m Bool
-finished = fmap isRight . resume
+finished = fmap isRight . resume . runParticle
 
 -- | Modify the transformed monad.
 -- This operation only applies to computation up to the first barrier.
 mapMonad :: Monad m =>
             (forall a. m a -> m a) -> Particle m a -> Particle m a
-mapMonad f cort = Coroutine {resume= f $ resume cort}
+mapMonad f = Particle . Coroutine . f . resume . runParticle
 
 type instance CustomReal (Particle m) = CustomReal m
 
