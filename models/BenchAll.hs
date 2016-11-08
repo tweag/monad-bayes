e22ddc33e3a71600a587d8d3cb0a3722b49f6223
Adam Scibior <ams240@cam.ac.uk>
2016-11-08 23:18:10 +0000

    Update benchmarking script

--- models/BenchAll.hs
+++ models/BenchAll.hs
@@ -10,24 +10,34 @@ import qualified DPmixture
 import qualified Gamma
 import qualified HMM
 
+import Control.Monad.Bayes.LogDomain
 import Control.Monad.Bayes.Class
 import Control.Monad.Bayes.Weighted
 import Control.Monad.Bayes.Sampler
-import qualified Control.Monad.Bayes.Dist as Dist
+import Control.Monad.Bayes.Dist
 import Control.Monad.Bayes.Inference
+import Control.Monad.Bayes.Empirical
 
 import System.IO
+import Control.Arrow (second)
+
+ns = [1..10]
 
 main = do
   -- make sure `putStrLn` prints to console immediately
   hSetBuffering stdout LineBuffering
 
-  --xs <- pimh 10 100 100 DPmixture.dpMemClusters
-  --putStrLn $ show $ (Dist.enumerate :: Dist.Dist Double Int -> [(Int,Double)]) $ uniformD xs
+  -- pimhSamples <- pimh 10 100 1000 DPmixture.dpMemClusters
+  -- let pimhResults = map ((`kl` DPmixture.posteriorClustersDist) . uniformD . (`take` pimhSamples) . (*100)) ns
+  -- putStrLn $ show $ pimhResults
+
+  smcSamples <- sequence $ replicate 10 $ fmap (map (second fromLogDomain)) $ runPopulation $ smc 16 100 $ fmap head HMM.hmm
+  let smcResults = map ((`kl` (head (tail HMM.exactMarginals))) . categorical . concat . (`take` smcSamples)) ns
+  putStrLn $ show $ smcResults
 
   --putStrLn $ show $ Dist.explicit $ DPmixture.posteriorClustersDist
 
-  (xs,w) <- runWeighted HMM.hmm
-  putStrLn $ show $ xs
+  --(xs,w) <- runWeighted HMM.hmm
+  --putStrLn $ show $ xs
 
-  putStrLn $ show $ map Dist.explicit $ HMM.exactMarginals
+  --putStrLn $ show $ map Dist.explicit $ HMM.exactMarginals
