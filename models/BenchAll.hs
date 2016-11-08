f2837984a7e0182493a6fce4e5f82e8bb4a7381d
Adam Scibior <ams240@cam.ac.uk>
2016-11-08 17:48:29 +0000

    Enable benchmarking

--- models/BenchAll.hs
+++ models/BenchAll.hs
@@ -10,10 +10,24 @@ import qualified DPmixture
 import qualified Gamma
 import qualified HMM
 
+import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Weighted
+import Control.Monad.Bayes.Sampler
+import qualified Control.Monad.Bayes.Dist as Dist
+import Control.Monad.Bayes.Inference
+
 import System.IO
 
 main = do
   -- make sure `putStrLn` prints to console immediately
   hSetBuffering stdout LineBuffering
 
-  putStrLn "Benchmark not implemented"
+  --xs <- pimh 10 100 100 DPmixture.dpMemClusters
+  --putStrLn $ show $ (Dist.enumerate :: Dist.Dist Double Int -> [(Int,Double)]) $ uniformD xs
+
+  --putStrLn $ show $ Dist.explicit $ DPmixture.posteriorClustersDist
+
+  (xs,w) <- runWeighted HMM.hmm
+  putStrLn $ show $ xs
+
+  putStrLn $ show $ map Dist.explicit $ HMM.exactMarginals
