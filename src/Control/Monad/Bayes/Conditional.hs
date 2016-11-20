e69468a016b3aad16b87e44f9561fcde84367000
Adam Scibior <adscib@gmail.com>
2016-11-20 22:58:44 +0000

    Move AutoDiff to the main package

--- src/Control/Monad/Bayes/Conditional.hs
+++ src/Control/Monad/Bayes/Conditional.hs
@@ -12,7 +12,9 @@ Portability : GHC
 {-# LANGUAGE
   GeneralizedNewtypeDeriving,
   TypeFamilies,
-  FlexibleContexts
+  FlexibleContexts,
+  FlexibleInstances,
+  RankNTypes
    #-}
 
 module Control.Monad.Bayes.Conditional (
@@ -21,11 +23,21 @@ module Control.Monad.Bayes.Conditional (
   pseudoDensity,
   jointDensity,
   contJointDensity,
-  unsafeContJointDensity
+  unsafeContJointDensity,
+  unsafeContJointDensityGradient
 ) where
 
 import Control.Monad.State
 import Data.Maybe
+import Control.Arrow (first)
+
+import Data.Reflection
+import Numeric.AD
+import Numeric.AD.Mode.Reverse
+import Numeric.AD.Internal.Reverse
+import Numeric.AD.Internal.Identity
+import Numeric.AD.Jacobian
+import qualified Numeric.SpecFunctions as Spec
 
 import Control.Monad.Bayes.LogDomain
 import Control.Monad.Bayes.Weighted
@@ -92,3 +104,17 @@ contJointDensity m xs = jointDensity m (xs,[])
 -- | Like 'contJointDensity', but throws an error if density can not be computed.
 unsafeContJointDensity :: MonadDist (Deterministic r) => Conditional (Weighted (Deterministic r)) a -> [r] -> LogDomain r
 unsafeContJointDensity m xs = fromMaybe (error "Could not compute density: some random variables are discrete or the list of random variables is too short") $ contJointDensity m xs
+
+
+-------------------------------------------------
+-- Automatic Differentiation
+
+instance (Reifies s Tape) => NumSpec (Reverse s Double) where
+  logGamma = lift1 logGamma (Id . Spec.digamma . runId)
+  logBeta  = lift2 logBeta (\(Id x) (Id y) -> (Id (psi x - psi (x+y)), Id (psi y - psi (x+y))))
+    where psi = Spec.digamma
+
+-- | Like 'unsafeContJointDensity', but additionally returns the gradient.
+-- Note that this is the gradient of the log-likelihood, even though it is not represented using 'LogDomain'.
+unsafeContJointDensityGradient :: (forall s. Reifies s Tape =>  Conditional (Weighted (Deterministic (Reverse s Double))) a) -> [Double] -> (LogDomain Double, [Double])
+unsafeContJointDensityGradient m xs = first fromLog $ grad' (toLog . unsafeContJointDensity m) xs
