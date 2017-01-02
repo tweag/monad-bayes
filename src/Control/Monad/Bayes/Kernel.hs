576c29d87ca06d9e73520b2950a1b467c9536275
Adam Scibior <adscib@gmail.com>
2017-01-02 20:12:26 +0100

    Implement Kernel

---
+++ src/Control/Monad/Bayes/Kernel.hs
@@ -0,0 +1,48 @@
+{-|
+Module      : Control.Monad.Bayes.Kernel
+Description : Kernels for use with kernel methods
+Copyright   : (c) Adam Scibior, 2017
+License     : MIT
+Maintainer  : ams240@cam.ac.uk
+Stability   : experimental
+Portability : GHC
+
+-}
+
+{-# LANGUAGE
+  GADTs
+ #-}
+
+
+module Control.Monad.Bayes.Kernel (
+  Kernel,
+  evalKernel,
+  unsafeKernel,
+  constant,
+  gaussian
+  ) where
+
+-- | A positive semidefinite real-valued symmetric function of two arguments.
+-- The first parameter is the type used to represent real numbers,
+-- the second parameter is input domain.
+newtype Kernel r a = Kernel (a -> a -> r)
+
+-- Evaluate the kernel at specific points.
+evalKernel :: Kernel r a -> a -> a -> r
+evalKernel (Kernel k) = k
+
+-- | Construct a kernel from an arbitrary function.
+-- No checks are performed to ensure that the function has the required properties.
+unsafeKernel :: (a -> a -> r) -> Kernel r a
+unsafeKernel = Kernel
+
+-- | A constant kernel that ignores inputs.
+constant :: (Ord r, Real r) => r -> Kernel r a
+constant c | c >= 0 = unsafeKernel $ const $ const c
+constant c = error $ "Attempted to construct a constant kernel of value " ++ show (toRational c)
+
+-- | A Gaussian RBF kernel with specified width.
+gaussian :: (Real r, Floating r) => r -> Kernel r r
+gaussian sigma | sigma >= 0 = unsafeKernel $ \x y ->
+                                exp (- (x - y)^2 / (2 * sigma^2))
+gaussian sigma = error $ "Attempted to construct gaussian kernel with width " ++ show (toRational sigma)
