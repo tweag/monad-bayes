0b2459d9fa582bc8c98f48bcf4c215cbedd94804
Adam Scibior <ams240@cam.ac.uk>
2017-02-05 19:39:24 +0000

    Implement linear Gaussian benchmark

--- models/LGSS.hs
+++ models/LGSS.hs
@@ -7,14 +7,83 @@ module Main where
 
 import Data.Vector (Vector, fromList, foldM, postscanl)
 import qualified Data.Vector as Vector
+import Data.Bifunctor (second)
+import Graphics.Rendering.Chart.Easy hiding (Vector)
+import Graphics.Rendering.Chart.Backend.Cairo (toFile, fo_format, FileFormat(PDF))
+import Options.Applicative
+import System.Directory
+import Control.Monad.Trans
+import System.IO
+import Control.Monad (when, unless)
 
 import Control.Monad.Bayes.Primitive
 import Control.Monad.Bayes.Class
 import Control.Monad.Bayes.Prior
+import Control.Monad.Bayes.Sampler
+import Control.Monad.Bayes.Population
+import Control.Monad.Bayes.Inference
 
 import Plotting
 
-main = return ()
+main = do
+  -- make sure `putStrLn` prints to console immediately
+  hSetBuffering stdout LineBuffering
+
+  (trial, cachePath) <- execParser opts
+  when trial $ putStrLn "Trial run"
+
+  if not trial then do
+    sampleIO $ lgssBenchmark cachePath 50 10 [100,500,1000]
+  else do
+    sampleIO $ lgssBenchmark cachePath 5 10 [10,20,40]
+
+opts :: ParserInfo (Bool,FilePath)
+opts = flip info fullDesc ((,) <$> trialFlag <*> cacheDir) where
+  trialFlag = switch
+    ( long "trial"
+    <> help "Run a quick version of benchmarks to check that all is working correctly.")
+  cacheDir = strOption
+    ( long "cache-dir"
+    <> value "cache/lgss/"
+    <> help "Directory to store temporary data that may be reused accross different runs.")
+
+tryCache :: (MonadIO m, Read a, Show a) => FilePath -> m a -> m a
+tryCache filepath fresh = do
+  exists <- liftIO $ doesFileExist filepath
+  if exists then
+    liftIO $ fmap read (readFile filepath)
+  else do
+    value <- fresh
+    liftIO $ writeFile filepath (show value)
+    return value
+
+lgssBenchmark :: FilePath -> Int -> Int -> [Int] -> SamplerIO ()
+lgssBenchmark cachePath t nRuns ns = do
+  liftIO $ putStrLn "running Nonlinear benchmark"
+
+  let dataPath = cachePath ++ "data.txt"
+  let refPath = cachePath ++ "reference.txt"
+  let scoresPath = cachePath ++ "scores.txt"
+  let plotPath = "lgss.pdf"
+  liftIO $ createDirectoryIfMissing True cachePath
+
+  let param = LGSSParam (0,1) 1 0 1 1 0 1
+  ys <- tryCache dataPath $ synthesizeData param t
+  ref <- tryCache refPath $ return $ kalman param ys
+  let run m = do -- RMSE on the mean of filetering distribution
+        estMean <- popAvg Vector.last (normalize m)
+        let trueMean = fst (Vector.last ref)
+        return $ abs (trueMean - estMean)
+  -- let run m = fmap ((/ fromIntegral nRuns) . sum) $ Vector.replicateM nRuns $
+  --             fmap (rmse ref . averageVec) $
+  --             explicitPopulation $ normalize m
+  scores <- tryCache scoresPath $
+            mapM (\n -> run $ smc t n (linearGaussian param ys)) ns
+
+  liftIO $ toFile (fo_format .~ PDF $ def) plotPath $ do
+    layout_title .= "LGSS"
+    anytimePlot "#samples" "RMSE" ns [
+      ("SMC", scores)]
 
 type Mean = Double
 type StdDev = Double
@@ -56,20 +125,20 @@ randomWalk :: (MonadBayes m, CustomReal m ~ Double)
 randomWalk p0 sdX sdY ys = linearGaussian (LGSSParam p0 1 0 sdX 1 0 sdY) ys
 
 -- | Generate observed sequence from the prior.
-generateData :: (MonadDist m, CustomReal m ~ Double)
+synthesizeData :: (MonadDist m, CustomReal m ~ Double)
              => LGSSParam
              -> Int -- ^ T - length of producted vector
              -> m (Vector Double) -- ^ data Y_{1:T} generated from the prior
-generateData param t = do
+synthesizeData param t = do
   xs <- prior $ linearGaussian param $ Vector.replicate t undefined
   Vector.mapM (\x -> normal (c param * x + d param) (sdY param)) xs
 
--- | Kalman filter computing exact filtering distribution X_t|Y{1_t} for each t.
+-- | Kalman filter computing exact filtering distributions X_t|Y_{1:t}.
 kalman :: LGSSParam
        -> Vector Double
        -> Vector (Mean, StdDev)
 kalman (LGSSParam (m0,sd0) a b sdX c d sdY) ys =
-  postscanl step (m0, var0) ys where
+  Vector.map (second sqrt) $ postscanl step (m0, var0) ys where
     var0 = sd0 * sd0
     varX = sdX * sdX
     varY = sdY * sdY
