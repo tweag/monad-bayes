af344b852667258e9beb6fa1dc10dfdea8e82640
Adam Scibior <ams240@cam.ac.uk>
2017-02-05 16:11:08 +0000

    Revert "Implement linear Gaussian model"
    This reverts commit bd6619d7cb56733c6e520ec82f28b07c8b2e8578.

--- models/SSM.hs
+++ models/SSM.hs
@@ -1,14 +1,12 @@
 
 {-# LANGUAGE
- RankNTypes,
- TypeFamilies
+ RankNTypes
  #-}
 
 module SSM where
 
 import qualified Data.Vector as Vector
 import Control.Monad.Trans
-import Control.Monad.Trans.Identity
 
 import Control.Monad.Bayes.Class
 import Control.Monad.Bayes.Trace
@@ -22,17 +20,16 @@ data SSM m a = SSM {generative :: Time -> m (Vector.Vector (a, CustomReal m)),
 type Model a = forall m. MonadBayes m => m a
 
 ssm :: MonadBayes m
-    => m a -> (Time -> a -> m a) -> (Time -> a -> forall t. (MonadTrans t, MonadBayes (t m), CustomReal m ~ CustomReal (t m)) => t m (CustomReal m))
+    => m a -> (Time -> a -> m a) -> (Time -> a -> Model (CustomReal m))
     -> SSM m a
 ssm initial trans obs = SSM gen post where
-
-  gen t = runIdentityT $ do
+  gen t = do
     let simulate 0 _ acc = return acc
         simulate n x acc = do
-          x' <- lift $ trans (t-n) x
-          y' <-        obs   (t-n) x'
+          x' <- trans (t-n) x
+          y' <- obs   (t-n) x'
           simulate (n-1) x' ((x',y'):acc)
-    x0 <- lift $ initial
+    x0 <- initial
     ps <- simulate t x0 []
     return $ Vector.fromList $ reverse ps
 
@@ -46,14 +43,3 @@ ssm initial trans obs = SSM gen post where
     x0 <- lift $ initial
     ps <- simulate t x0 []
     return $ Vector.fromList $ reverse ps
-
-type Mean r = r
-type StdDev r = r
-type Linear r = (r,r)
-
-linearGaussian :: (MonadBayes m , CustomReal m ~ r)
-               => (Mean r, StdDev r) -> (Linear r, StdDev r) -> StdDev r
-               -> SSM m r
-linearGaussian initial ((a,b),diff) noise =
-  ssm (uncurry normal initial) (const (\x -> normal (a*x + b) diff))
-      (const (\x -> normal x noise))
