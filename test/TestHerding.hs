384afec97dc627d30280187cdda840b250196cdd
Adam Scibior <adscib@gmail.com>
2017-02-06 01:15:28 +0000

    Rename normalize in TestHerding

--- test/TestHerding.hs
+++ test/TestHerding.hs
@@ -22,12 +22,12 @@ spec = do
 
 prop_herdingGreedy :: Kernel R R -> [(R,R)] -> Int -> Property
 prop_herdingGreedy k p' n = not (null p') && sum (map snd p') /= 0 && n>0 ==> greedy ~== eval (x:xs) where
-  p = normalize p'
+  p = normalize' p'
   x:xs = reverse $ take n $ herding k p
   greedy = minimum [eval (y:xs) | y <- fst (unzip p)]
   eval ys = mmd k p $ map (\y -> (y, 1 / fromIntegral (length ys))) ys
 
 -- | Normalize the weights to sum to 1.
-normalize :: Fractional p => [(a,p)] -> [(a,p)]
-normalize xs = map (second (/ z)) xs where
+normalize' :: Fractional p => [(a,p)] -> [(a,p)]
+normalize' xs = map (second (/ z)) xs where
   z = sum $ map snd xs
