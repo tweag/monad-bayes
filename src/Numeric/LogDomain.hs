09a1b04784d31fd814459fc00d89042d35161a17
Adam Scibior <adscib@gmail.com>
2017-03-31 12:49:37 +0200

    Fix edge cases in LogDomain

--- src/Numeric/LogDomain.hs
+++ src/Numeric/LogDomain.hs
@@ -9,13 +9,19 @@ Portability : GHC
 
 -}
 
--- Log-domain non-negative real numbers
--- Essentially a polymorphic version of LogFloat to allow for AD
+-- Log-domain non-negative real numbers.
+-- Essentially a polymorphic version of LogFloat to allow for AD.
+-- Unlike LogFloat, we do not attempt to fix any problems with standard floating-point numbers,
+-- in particular NaN is a legal value in LogDomain and will usually propagate silently.
+-- In most cases +Inf behaves in the usual way, the only exception being that addition involving +Inf results in NaN.
+-- Since LogDomain can not represent negative numbers, -Inf is not a legal value.
+-- Zeroes are represented internally as -Inf but this should be transpartent to the user.
+-- Note that if the base type does not have -Inf then LogDomain can not represent zeros.
 module Numeric.LogDomain (
   LogDomain,
   toLogDomain,
   fromLogDomain,
-  mapLog,
+  mapLogDomain,
   toLog,
   fromLog,
   liftLog,
@@ -40,41 +46,45 @@ import Numeric.AD.Internal.Identity
 import Numeric.AD.Jacobian
 
 -- | Log-domain non-negative real numbers.
--- Used to prevent underflow when computing small probabilities.
+-- Used to prevent under- and overflow.
 newtype LogDomain a = LogDomain a
-  deriving (Eq, Ord)
+  deriving (Eq, Ord, Show, Read)
 
 -- | Convert to log-domain.
-toLogDomain :: Floating a => a -> LogDomain a
-toLogDomain = fromLog . log
+-- Modulo floating-point errors this is an isomorphism with respect to any numeric operations.
+-- The inverse is given by `fromLogDomain`.
+toLogDomain :: (Ord a, Floating a) => a -> LogDomain a
+toLogDomain x =
+  if x < 0 then
+    error "LogDomain: attempted to convert a negative number to LogDomain"
+  else
+    fromLog $ log x
 
 -- | Inverse of `toLogDomain`.
---
--- > toLogDomain . fromLogDomain = id
--- > fromLogDomain . toLogDomain = id
 fromLogDomain :: Floating a => LogDomain a -> a
 fromLogDomain = exp . toLog
 
 -- | Apply a function to a log-domain value by first exponentiating,
 -- then applying the function, then taking the logarithm.
 --
--- > mapLog f = toLogDomain . f . fromLogDomain
-mapLog :: (Floating a, Floating b) => (a -> b) -> LogDomain a -> LogDomain b
-mapLog f = toLogDomain . f . fromLogDomain
+-- > mapLogDomain f = toLogDomain . f . fromLogDomain
+mapLogDomain :: (Ord b, Floating a, Floating b) => (a -> b) -> LogDomain a -> LogDomain b
+mapLogDomain f = toLogDomain . f . fromLogDomain
 
 -- | A raw representation of a logarithm.
---
+-- Modulo floating-point errors the following holds.
 -- > toLog = log . fromLogDomain
 toLog :: LogDomain a -> a
 toLog (LogDomain x) = x
 
 -- | Inverse of `toLog`.
---
+-- Modulo floating-point errors the following holds.
 -- > fromLog = toLogDomain . exp
 fromLog :: a -> LogDomain a
 fromLog = LogDomain
 
 -- | Apply a function to a logarithm.
+-- Avoids unnecessarily applying `log` `.` `exp`.
 --
 -- > liftLog f = fromLog . f . toLog
 liftLog :: (a -> b) -> LogDomain a -> LogDomain b
@@ -86,18 +96,35 @@ liftLog f = fromLog . f . toLog
 liftLog2 :: (a -> b -> c) -> LogDomain a -> LogDomain b -> LogDomain c
 liftLog2 f x y = fromLog $ f (toLog x) (toLog y)
 
-instance Show a => Show (LogDomain a) where
-  show = show . toLog
-
 instance (Ord a, Floating a) => Num (LogDomain a) where
-  x + y = if y > x then y + x
-                   else fromLog (toLog x + log (1 + exp (toLog y - toLog x)))
-  x - y = if x >= y then fromLog (toLog x + log (1 - exp (toLog y - toLog x)))
-                    else error "LogDomain: subtraction resulted in a negative value"
+  x + y =
+    -- if one of inputs is +inf, the result is NaN
+    let m = max x y in
+      if m == 0 then
+        -- can't just return 0 here as one of inputs can be NaN
+        toLogDomain $ fromLogDomain x + fromLogDomain y
+      else
+        -- first divide by m to avoid under- and overflow
+        m * (toLogDomain $ fromLogDomain (x / m) + fromLogDomain (y / m))
+  x - y =
+    if y > x then
+      error "LogDomain: subtraction resulted in a negative value"
+    else
+      if x == 0 then
+        -- unlike just returning zero, this ensures that NaNs propagate through
+        negate y
+      else
+        -- numerically stable subtraction
+        x * (toLogDomain $ 1 - fromLogDomain (y / x))
   (*) = liftLog2 (+)
-  negate _ = error "LogDomain does not support negation"
+  negate x =
+    if x > 0 then
+      error "LogDomain: attempted to negate a positive number"
+    else
+      -- unlike just returning zero, this ensures that NaNs propagate through
+      toLogDomain $ negate $ fromLogDomain x
   abs = id
-  signum x = if toLog x == -1/0 then 0 else 1
+  signum = toLogDomain . signum . fromLogDomain -- not the fastest way, but preserves NaNs
   fromInteger = toLogDomain . fromInteger
 
 instance (Real a, Floating a) => Real (LogDomain a) where
@@ -111,22 +138,34 @@ instance (Ord a, Floating a) => Fractional (LogDomain a) where
 instance (Ord a, Floating a) => Floating (LogDomain a) where
   pi = toLogDomain pi
   exp = liftLog exp
-  log = liftLog log
+  log = liftLog $ \x ->
+    if x < 0 then
+      error "LogDomain: attempted to take log of a value smaller than 1"
+    else
+      log x
   sqrt = liftLog (/ 2)
   x ** y = liftLog (* fromLogDomain y) x
-  logBase x y = log y / log x
-  sin   = mapLog sin
-  cos   = mapLog cos
-  tan   = mapLog tan
-  asin  = mapLog asin
-  acos  = mapLog acos
-  atan  = mapLog atan
-  sinh  = mapLog sinh
-  cosh  = mapLog cosh
-  tanh  = mapLog tanh
-  asinh = mapLog asinh
-  acosh = mapLog acosh
-  atanh = mapLog atanh
+  logBase x y =
+    if x < 1 then
+      -- if both arguments are smaller than 1 then the result may be positive even if natural logs are not
+      log (recip y) / log (recip x)
+    else
+      log y / log x
+  sin   = mapLogDomain sin
+  cos   = mapLogDomain cos
+  tan   = mapLogDomain tan
+  asin  = mapLogDomain asin
+  acos  = mapLogDomain acos
+  atan  = mapLogDomain atan
+  sinh  = mapLogDomain sinh
+  cosh  = mapLogDomain cosh
+  tanh  = mapLogDomain tanh
+  asinh = mapLogDomain asinh
+  acosh = mapLogDomain acosh
+  atanh = mapLogDomain atanh
+
+-------------------------------------------------
+-- NumSpec class
 
 -- | A class for numeric types that provide certain special functions.
 class Floating a => NumSpec a where
@@ -180,7 +219,11 @@ instance (Ord a, NumSpec a) => NumSpec (LogDomain a) where
   -- to prevent overflow all numbers are first divided by their maximum,
   -- and the final result is mulitplied by it
   sum t = let z = Fold.maximum t in
-    if z == 0 then 0 else  z * toLogDomain (Fold.foldl' (\x y -> x + fromLogDomain (y / z)) 0 t)
+    if z == 0 then
+      -- unlike just returning zero, this ensures that NaNs propagate through
+      toLogDomain (Fold.foldl' (\x y -> x + fromLogDomain y) 0 t)
+    else
+      z * toLogDomain (Fold.foldl' (\x y -> x + fromLogDomain (y / z)) 0 t)
 
   -- just like default, but with extra checks
   normalize t = Traverse.fmapDefault (/ z) t where
@@ -194,3 +237,4 @@ instance (Reifies s Tape) => NumSpec (Reverse s Double) where
   logGamma = lift1 logGamma (Id . Spec.digamma . runId)
   logBeta  = lift2 logBeta (\(Id x) (Id y) -> (Id (psi x - psi (x+y)), Id (psi y - psi (x+y))))
     where psi = Spec.digamma
+  -- TODO: can we check the weights in normalize here?
