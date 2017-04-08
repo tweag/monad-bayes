34102ebe0a4f4b727dcdbcaa572971b228ce307f
Adam Scibior <adscib@gmail.com>
2017-04-08 12:10:09 +0200

    Specialize Reparametrized to Reverse

--- src/Control/Monad/Bayes/Reparametrized.hs
+++ src/Control/Monad/Bayes/Reparametrized.hs
@@ -16,8 +16,10 @@ module Control.Monad.Bayes.Reparametrized (
 ) where
 
 import Data.Vector
+import Data.Reflection (Reifies)
 import Numeric.AD
 import Numeric.AD.Mode.Reverse
+import Numeric.AD.Internal.Reverse (Tape)
 import Control.Monad.Trans (MonadTrans, MonadIO, lift)
 import Control.Monad.Trans.Identity (IdentityT(IdentityT))
 import Control.Monad.Reader (ask)
@@ -30,34 +32,35 @@ import qualified Statistics.Distribution.Polymorphic.Uniform as U
 import Control.Monad.Bayes.Simple
 import Control.Monad.Bayes.Sampler
 
-newtype Reparametrized t m a = Reparametrized (IdentityT m a)
+-- | A transformer that uses the reparametrization trick to automatically propagate gradients through sampling functions.
+newtype Reparametrized s m a = Reparametrized (IdentityT m a)
   deriving(Read, Show, Eq, Functor, Applicative, Monad, MonadTrans, MonadIO)
 
-instance (HasCustomReal m, CustomReal m ~ Scalar t, Floating t, Ord t) => HasCustomReal (Reparametrized t m) where
-  type CustomReal (Reparametrized t m) = t
+instance (HasCustomReal m, Reifies s Tape) => HasCustomReal (Reparametrized s m) where
+  type CustomReal (Reparametrized s m) = Reverse s (CustomReal m)
 
-instance (Functor m, Floating t, Mode t, HasCustomReal m, Scalar t ~ CustomReal m,
-          Sampleable (Normal (Scalar t)) m) => Sampleable (Normal t) (Reparametrized t m) where
+instance (Functor m, HasCustomReal m, r ~ CustomReal m, Reifies s Tape, Sampleable (Normal r) m)
+        => Sampleable (Normal (Reverse s r)) (Reparametrized s m) where
   sample d = Reparametrized $ IdentityT $ fmap (\x -> (auto x - N.mean d) / N.stddev d) $ normal 0 1
 
-instance (Functor m, Floating t, Mode t, HasCustomReal m, Scalar t ~ CustomReal m,
-          Sampleable (Uniform (Scalar t)) m) => Sampleable (Uniform t) (Reparametrized t m) where
+instance (Functor m, HasCustomReal m, r ~ CustomReal m, Reifies s Tape, Sampleable (Uniform r) m)
+        => Sampleable (Uniform (Reverse s r)) (Reparametrized s m) where
   sample d = Reparametrized $ IdentityT $ fmap (\x -> a + (b - a) * (auto x)) $ uniform 0 1 where
     a = U.lower d
     b = U.upper d
 
-instance (Functor m, Floating t, Ord t, Mode t, HasCustomReal m, Scalar t ~ CustomReal m,
-          Sampleable (Uniform (Scalar t)) m) => Sampleable (Discrete t Int) (Reparametrized t m) where
+instance (Functor m, HasCustomReal m, r ~ CustomReal m, Reifies s Tape, Sampleable (Uniform r) m)
+        => Sampleable (Discrete (Reverse s r) Int) (Reparametrized s m) where
   sample d = Reparametrized $ IdentityT $ fmap (select . auto) $ uniform 0 1 where
     select x = case findIndex (>= x) (scanl1' (+) (D.weights d)) of
                   Just i -> i
                   Nothing -> error "Reparametrized: bad weights in Discrete"
 
 -- | Reparametrize model to sample only from distributions with fixed parameters.
-reparametrize :: Reparametrized t m a -> m a
+reparametrize :: Reparametrized s m a -> m a
 reparametrize (Reparametrized (IdentityT m)) = m
 
 -- | Stochastic gradient using a single sample
-sg :: (forall s. [Reverse s Double] -> Reparametrized (Reverse s Double) SamplerST (Reverse s Double))
-   -> [Double] -> SamplerST [Double]
+sg :: (Num r, Functor m, Traversable t) => (forall s. t (Reverse s r) -> Reparametrized s m (Reverse s r))
+   -> t r -> m (t r)
 sg model = gradM (reparametrize . model)
