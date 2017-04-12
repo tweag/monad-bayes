daf3e8b7638f1c2a8179f88bd9dbf4d199c263f2
Adam Scibior <adscib@gmail.com>
2017-04-12 12:30:01 +0200

    Implement ADVI

--- src/Control/Monad/Bayes/Inference/Variational.hs
+++ src/Control/Monad/Bayes/Inference/Variational.hs
@@ -12,10 +12,13 @@ Portability : GHC
 module Control.Monad.Bayes.Inference.Variational (
   elbo,
   elboGrad,
-  -- elboWithGrad,
-  optimizeELBO
+  optimizeELBO,
+  advi
 ) where
 
+import Prelude hiding (zip, splitAt, length, (++))
+
+import Data.Vector
 import Numeric.AD.Mode.Reverse
 
 import Numeric.LogDomain
@@ -24,6 +27,8 @@ import Control.Monad.Bayes.Simple hiding (Parametric)
 import Control.Monad.Bayes.Weighted
 import Control.Monad.Bayes.Reparametrized
 import Control.Monad.Bayes.Parametric
+import Control.Monad.Bayes.MeanField as MF
+import Control.Monad.Bayes.Constraint
 
 -- | Estimator for evidence lower bound.
 elbo :: (HasCustomReal m, Functor m) => Weighted m a -> m (CustomReal m)
@@ -44,3 +49,20 @@ optimizeELBO :: (Traversable t, HasCustomReal m, Monad m)
              -> t (CustomReal m) -- ^ initial values of parameters
              -> m (t (CustomReal m))
 optimizeELBO model optParam initial = sga optParam (fmap snd . elboGrad model) initial
+
+reshapeParam :: Parametric (Vector (a, a)) m b -> Parametric (Vector a) m b
+reshapeParam m = parametric (withParam m . split) where
+  split v = uncurry zip $ splitAt (length v `div` 2) v
+
+advi :: (MonadDist m', MonadDist m'', CustomReal m' ~ CustomReal m'')
+     => Int -- ^ number of random variables in the model
+     -> (forall s. MeanFieldNormal (Constraint (Weighted (Reparametrized s m'))) a) -- ^ model
+     -> MeanFieldNormal (Constraint (Weighted m'')) a -- ^ same model in a different monad
+     -> SGDParam (CustomReal m'')
+     -> m' (m'' a)
+advi n m m' sgdParam = do
+  initialMeans <- replicateM n (uniform (-1) 1)
+  initialStdDevs <- replicateM n (uniform 0.01 1)
+  let initialParam = initialMeans ++ initialStdDevs
+  bestParam <- optimizeELBO (reshapeParam $ meanFieldNormal $ MF.hoist unconstrain m) sgdParam initialParam
+  return $ prior $ (withParam $ reshapeParam $ meanFieldNormal $ MF.hoist unconstrain m') bestParam
