83c5f5691180e806571809d5fd29a8a2229a2e86
Adam Scibior <ams240@cam.ac.uk>
2017-06-26 14:15:14 +0100

    Add sampling benchmarks

--- models/Benchmarks.hs
+++ models/Benchmarks.hs
@@ -1,14 +1,17 @@
 import Criterion.Main
 import Numeric.AD
+import System.Random.MWC (createSystemRandom, GenIO)
 
 import Numeric.LogDomain
 import Statistics.Distribution.Polymorphic
+import Control.Monad.Bayes.Simple
+import Control.Monad.Bayes.Sampler
 
 pdfBenchmark :: (Density d) => d -> Domain d -> Benchmark
-pdfBenchmark d x = bench "pdf" $ whnf (toLog . pdf d) x
+pdfBenchmark d x = bench "density" $ whnf (toLog . pdf d) x
 
-main :: IO ()
-main = defaultMain [
+pdfBenchmarks :: Benchmark
+pdfBenchmarks = bgroup "pdf" [
   bgroup "normal" [ pdfBenchmark (normalDist 1 5) (4 :: Double),
                     bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 1 5) . head )) [4 :: Double]
                   ],
@@ -22,3 +25,26 @@ main = defaultMain [
                     bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 7 10) . head )) [8 :: Double]
                   ]
   ]
+
+samplingBenchmarks :: GenIO -> Benchmark
+samplingBenchmarks g = bgroup "sample" [
+  bench "normal"  $ whnfIO $ sampleIOwith (normal 1 5)   g,
+  bench "gamma"   $ whnfIO $ sampleIOwith (gamma 2 3)    g,
+  bench "beta"    $ whnfIO $ sampleIOwith (beta 2 4)     g,
+  bench "uniform" $ whnfIO $ sampleIOwith (uniform 7 10) g
+  ]
+
+
+
+
+main :: IO ()
+main = do
+
+  g <- createSystemRandom
+
+  let benchmarks = [
+        pdfBenchmarks,
+        samplingBenchmarks g
+        ]
+
+  defaultMain benchmarks
