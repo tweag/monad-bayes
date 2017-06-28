efa24861b53871bf9f974845c5cc3f6ca0c40f56
Adam Scibior <ams240@cam.ac.uk>
2017-06-28 11:16:05 +0100

    Add benchmarks for discrete distributions

--- models/Benchmarks.hs
+++ models/Benchmarks.hs
@@ -7,8 +7,20 @@ import Statistics.Distribution.Polymorphic
 import Control.Monad.Bayes.Simple
 import Control.Monad.Bayes.Sampler
 
+pdfBenchmarkable :: (Density d) => d -> Domain d -> Benchmarkable
+pdfBenchmarkable d = whnf (toLog . pdf d)
+
 pdfBenchmark :: (Density d) => d -> Domain d -> Benchmark
-pdfBenchmark d x = bench "density" $ whnf (toLog . pdf d) x
+pdfBenchmark d x = bench "density" $ pdfBenchmarkable d x
+
+benchN :: String -> [Int] -> (Int -> Benchmarkable) -> Benchmark
+benchN name ns bmark = bgroup name $ map (\n -> bench (show n) (bmark n)) ns
+
+defNs :: [Int]
+defNs = [1,10]
+
+defDiscrete :: Int -> Discrete Double
+defDiscrete n = discreteDist $ replicate n 1
 
 pdfBenchmarks :: Benchmark
 pdfBenchmarks = bgroup "pdf" [
@@ -23,7 +35,8 @@ pdfBenchmarks = bgroup "pdf" [
                   ],
   bgroup "uniform" [ pdfBenchmark (uniformDist 7 10) (8 :: Double),
                     bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 7 10) . head )) [8 :: Double]
-                  ]
+                  ],
+  benchN "discrete" defNs (\n -> pdfBenchmarkable (defDiscrete n) 0)
   ]
 
 samplingBenchmarks :: GenIO -> Benchmark
@@ -31,10 +44,9 @@ samplingBenchmarks g = bgroup "sample" [
   bench "normal"  $ whnfIO $ sampleIOwith (normal 1 5)   g,
   bench "gamma"   $ whnfIO $ sampleIOwith (gamma 2 3)    g,
   bench "beta"    $ whnfIO $ sampleIOwith (beta 2 4)     g,
-  bench "uniform" $ whnfIO $ sampleIOwith (uniform 7 10) g
+  bench "uniform" $ whnfIO $ sampleIOwith (uniform 7 10) g,
+  benchN "discrete" defNs (\n -> whnfIO $ sampleIOwith (sample $ defDiscrete n) g)
   ]
-
-
 
 
 main :: IO ()
