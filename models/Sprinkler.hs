47a0bd6bdc5aac5fbe201a91571e6088dffd098e
Adam Scibior <adscib@gmail.com>
2017-08-22 12:42:47 +0200

    Update TestWeighted

--- models/Sprinkler.hs
+++ models/Sprinkler.hs
@@ -3,9 +3,8 @@ module Sprinkler where
 import Control.Monad (when)
 
 import Control.Monad.Bayes.Class
-import Control.Monad.Bayes.Simple
 
-hard :: MonadBayes m => m Bool
+hard :: MonadInfer m => m Bool
 hard = do
   rain <- bernoulli 0.3
   sprinkler <- bernoulli $ if rain then 0.1 else 0.4
@@ -13,10 +12,10 @@ hard = do
                                               (True,False) -> 0.8
                                               (False,True) -> 0.9
                                               (False,False) -> 0.0
-  condition (wet == False)
+  condition (not wet)
   return rain
 
-soft :: MonadBayes m => m Bool
+soft :: MonadInfer m => m Bool
 soft = do
   rain <- bernoulli 0.3
   when rain (factor 0.2)
--- test/TestWeighted.hs
+++ test/TestWeighted.hs
@@ -4,34 +4,31 @@
 
 module TestWeighted where
 
-import Test.Hspec
 import Data.AEq
 import Control.Monad.State
-import Data.List
 import Data.Bifunctor (second)
+import Numeric.Log
 
-import Numeric.LogDomain (LogDomain, fromLogDomain, toLogDomain)
 import Control.Monad.Bayes.Class
-import Control.Monad.Bayes.Simple
 import Control.Monad.Bayes.Sampler
 import Control.Monad.Bayes.Weighted
-import Control.Monad.Bayes.Inference
 
-model :: (MonadBayes m, CustomReal m ~ Double) => m (Int,Double)
+model :: MonadInfer m => m (Int,Double)
 model = do
   n <- uniformD [0,1,2]
   unless (n == 0) (factor 0.5)
-  x <- if (n == 0) then return 1 else normal 0 1
-  when (n == 2) (factor $ toLogDomain (x*x))
+  x <- if n == 0 then return 1 else normal 0 1
+  when (n == 2) (factor $ (Exp . log) (x*x))
   return (n,x)
 
-result :: (MonadDist m, CustomReal m ~ Double) => m ((Int,Double), Double)
-result = fmap (second fromLogDomain) $ runWeighted model
+result :: MonadSample m => m ((Int,Double), Double)
+result = second (exp . ln) <$> runWeighted model
 
+passed :: IO Bool
 passed = fmap check (sampleIOfixed result)
 
 check :: ((Int,Double), Double) -> Bool
 check ((0,1),1) = True
-check ((1,x),y) =  y ~== 0.5
+check ((1,_),y) =  y ~== 0.5
 check ((2,x),y) =  y ~== 0.5 * x * x
 check _ = False
