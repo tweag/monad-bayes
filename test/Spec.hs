5096e2e840ffe550f403b0b317733917452e91ff
Adam Scibior <ams240@cam.ac.uk>
2017-08-29 11:58:28 +0100

    Clean up

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,25 +6,21 @@ import qualified TestWeighted
 import qualified TestEnumerator
 import qualified TestPopulation
 import qualified TestSequential
--- import qualified TestTrace
 import qualified TestInference
 
 
 main :: IO ()
 main = hspec $ do
-  describe "Weighted" $ do
+  describe "Weighted" $
     it "accumulates likelihood correctly" $ do
       passed <- TestWeighted.passed
       passed `shouldBe` True
   describe "Dist" $ do
-    -- we no longer normalize weights in categorical
-    -- it "normalizes categorical" $ do
-    --   TestEnumerator.passed1 `shouldBe` True
-    it "sorts samples and aggregates weights" $ do
+    it "sorts samples and aggregates weights" $
       TestEnumerator.passed2 `shouldBe` True
-    it "gives correct answer for the sprinkler model" $ do
+    it "gives correct answer for the sprinkler model" $
       TestEnumerator.passed3 `shouldBe` True
-    it "computes expectation correctly" $ do
+    it "computes expectation correctly" $
       TestEnumerator.passed4 `shouldBe` True
   describe "Population" $ do
     context "controlling population" $ do
@@ -34,7 +30,7 @@ main = hspec $ do
       it "multiplies the number of samples when spawn invoked twice" $ do
         manySize <- TestPopulation.manySize
         manySize `shouldBe` 15
-      it "correctly computes population average" $ do
+      it "correctly computes population average" $
         TestPopulation.popAvgCheck `shouldBe` True
     context "distribution-preserving transformations" $ do
       it "collapse preserves the distribution" $ do
@@ -48,62 +44,19 @@ main = hspec $ do
       TestSequential.checkTwoSync 0 `shouldBe` True
       TestSequential.checkTwoSync 1 `shouldBe` True
       TestSequential.checkTwoSync 2 `shouldBe` True
-    it "preserves the distribution" $ do
+    it "preserves the distribution" $
       TestSequential.checkPreserve `shouldBe` True
     it "produces correct intermediate weights" $ do
       TestSequential.checkSync 0 `shouldBe` True
       TestSequential.checkSync 1 `shouldBe` True
       TestSequential.checkSync 2 `shouldBe` True
-  -- describe "Trace" $ do
-  --   context "RandomDB = [Cache]" $ do
-  --     it "correctly records values" $ do
-  --       TestTrace.check_writing `shouldBe` True
-  --     it "correctly reuses values" $ do
-  --       TestTrace.check_reading `shouldBe` True
-  --   it "has reuse ratio 1 on an empty database" $ do
-  --     TestTrace.check_reuse_ratio TestTrace.m            `shouldBe` True
-  --     TestTrace.check_reuse_ratio TestSequential.sprinkler `shouldBe` True
---   describe "Density" $ do
---     it "correctly evaluates conditional distribution" $ do
---       TestConditional.check_missing_conditional `shouldBe` True
---       TestConditional.check_longer_conditional `shouldBe` True
---     it "correctly computes pseudo-marginal density" $ do
---       TestConditional.check_first_density `shouldBe` True
---     it "correctly computes joint density" $ do
---       TestConditional.check_joint_density_true `shouldBe` True
---       TestConditional.check_joint_density_false `shouldBe` True
   describe "SMC" $ do
-    it "terminates" $ do
+    it "terminates" $
       seq TestInference.checkTerminateSMC () `shouldBe` ()
-    it "preserves the distribution on the sprinkler model" $ do
+    it "preserves the distribution on the sprinkler model" $
       TestInference.checkPreserveSMC `shouldBe` True
     prop "number of particles is equal to its second parameter" $
       \observations particles ->
         observations >= 0 && particles >= 1 ==> ioProperty $ do
           checkParticles <- TestInference.checkParticles observations particles
           return $ checkParticles == particles
---   describe "MH" $ do
---     -- it "MH from prior leaves posterior invariant" $ do
---     --   TestInference.check_prior_trans `shouldBe` True
---     -- it "Trace MH produces correct number of samples" $ do
---     --   trace_mh_length <- TestInference.trace_mh_length 11
---     --   trace_mh_length `shouldBe` 11
---     -- it "Trace MH leaves posterior invariant" $ do
---     --   TestInference.check_trace_trans `shouldBe` True
---     -- it "Trace MH leaves posterior invariant when the model has shifting support" $ do
---     --   TestInference.check_trace_support `shouldBe` True
---     it "Custom MH preserves posterior distribution" $ do
---       TestInference.check_custom_mh `shouldBe` True
---     it "proposing from prior preserves posterior distribution" $ do
---       TestInference.check_prior_mh `shouldBe` True
---   -- describe "Population/Trace/Particle hybrids" $ do
---   --   it "ISMH preserves the posterior on the sprinkler model" $ do
---   --     TestInference.checkPreserve_ismh `shouldBe` True
---   --   it "SMH preserves the posterior on the sprinkler model" $ do
---   --     TestInference.checkPreserve_smh `shouldBe` True
---   --   it "Resample-move SMC preserves the posterior on the sprinkler model" $ do
---   --     TestInference.checkPreserve_smcrm `shouldBe` True
---
---     -- too large to execute
---     -- it "PIMH leaves posterior invariant" $ do
---     --   TestInference.check_pimh_trans `shouldBe` True
--- test/TestInference.hs
+++ test/TestInference.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE
-  TupleSections,
   Rank2Types,
   TypeFamilies
  #-}
@@ -7,17 +6,13 @@
 module TestInference where
 
 import Data.AEq
-import Control.Monad.Trans.Identity
 
 import Control.Monad.Bayes.Class
 import Control.Monad.Bayes.Enumerator
 import Control.Monad.Bayes.Sampler
-import Control.Monad.Bayes.Weighted hiding (prior)
 import Control.Monad.Bayes.Population
-import Control.Monad.Bayes.Traced
 import Control.Monad.Bayes.Inference
 import Sprinkler
-import qualified StrictlySmallerSupport
 
 sprinkler :: MonadInfer m => m Bool
 sprinkler = Sprinkler.soft
@@ -33,51 +28,3 @@ checkTerminateSMC = sampleIOfixed (smcMultinomial' 2 5 sprinkler)
 checkPreserveSMC :: Bool
 checkPreserveSMC = (enumerate . collapse . smcMultinomial 2 2) sprinkler ~==
                       enumerate sprinkler
-
--- checkPreserve_ismh = (enumerate . collapse . ismh 1 2) sprinkler ~==
---                       enumerate sprinkler
---
--- checkPreserveSmh = (enumerate . collapse . smh 1 2) sprinkler ~==
---                       enumerate sprinkler
---
--- checkPreserveSMCrm = (enumerate . collapse . SMCrm 1 2 1) sprinkler ~==
---                         enumerate sprinkler
-
--- sprinkler_posterior :: MonadInfer m => Weighted m Bool
--- sprinkler_posterior = duplicateWeight sprinkler
-
--- mhPriorrans :: MonadDist m => Weighted m Bool -> m Bool
--- mhPriorrans d = fmap (!! 1) $ mh 2 d (MHKernel $ const $ fmap (,1) sprinkler)
-
--- checkPriorTrans = enumerate (fmap (!! 2) (mhPrior 2 sprinkler_posterior)) ~==
---                     enumerate sprinkler
-
--- pimhTrans :: MonadDist m => Weighted m Bool -> m Bool
--- pimhTrans d = fmap (!! 1) $ mh 2 d kernel where
---   kernel = MHKernel $ const $ fmap (,1) $ collapse $ SMC 2 2 sprinkler
-
--- checkPimhTrans = enumerate (fmap (!! 2) (pimh 2 2 2 sprinkler_posterior)) ~==
---                    enumerate sprinkler
-
--- checkTrace_mh m m' = enumerate (dropTrace (mhStep (mhStep m))) ~==
---                       enumerate m'
---
--- trace_mh_length n = fmap length (sampleIOfixed (traceMH n sprinkler))
---
--- checkTraceTrans = checkTrace_mh sprinkler sprinkler
---
--- checkTraceSupport = checkTrace_mh StrictlySmallerSupport.model StrictlySmallerSupport.model
-
--- custom_mhTest :: (MHKernel k, KernelDomain k ~ Trace Double, MHSampler k ~ Dist.Enumerator (Deterministic Double)) => k -> [(Bool, Double)]
--- custom_mhTest k = enumerate (s >>= \x -> (fmap (!! 0) (runMCMC (mh sprinkler k) 1 x)))  where
---   s = marginal $ joint sprinkler
---
--- custom_mhTarget = enumerate sprinkler
---
--- check_custom_mh =
---   enumerate sprinkler ~==
---     custom_mhTest (singleSiteTraceKernel 0 identityKernel (discreteKernel (replicate 2 [0.5,0.5])))
---
--- checkPrior_mh =
---   enumerate sprinkler ~==
---     custom_mhTest (mhPriorKernel $ prior $ sprinkler >> return ())
