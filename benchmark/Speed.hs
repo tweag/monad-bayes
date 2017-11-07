428926207b8bb8190f0a8b4377cef9903b9af450
Adam Scibior <ams240@cam.ac.uk>
2017-11-07 10:59:19 +0000

    Update speed benchmark

--- benchmark/Speed.hs
+++ benchmark/Speed.hs
@@ -1,82 +1,40 @@
 import Criterion.Main
-import Numeric.AD
+import Numeric.Log
 import System.Random.MWC (createSystemRandom, GenIO)
 import Control.Monad.Par.Class (NFData)
 import Data.Bifunctor (second)
 
-import Numeric.LogDomain
-import Statistics.Distribution.Polymorphic
-import Control.Monad.Bayes.Simple
 import Control.Monad.Bayes.Sampler
 import Control.Monad.Bayes.Weighted
-import Control.Monad.Bayes.Inference hiding (mhStep)
+import Control.Monad.Bayes.Inference
 import Control.Monad.Bayes.Population
 import Control.Monad.Bayes.Sequential
 import Control.Monad.Bayes.Traced
-import Control.Monad.Bayes.Inference.MCMC (randomWalkKernel)
-import Control.Monad.Bayes.Constraint
 
-import NonlinearSSM
+-- import NonlinearSSM
 import qualified HMM
 import qualified LogReg
 import qualified LDA
 
-pdfBenchmarkable :: (Density d) => d -> Domain d -> Benchmarkable
-pdfBenchmarkable d = whnf (toLog . pdf d)
-
-pdfBenchmark :: (Density d) => d -> Domain d -> Benchmark
-pdfBenchmark d x = bench "density" $ pdfBenchmarkable d x
+defNs :: [Int]
+defNs = [100]
 
 benchN :: String -> [Int] -> (Int -> Benchmark) -> Benchmark
 benchN name ns bmark = bgroup name $ map (\n -> bgroup (show n) [bmark n]) ns
 
-defNs :: [Int]
-defNs = [100]
-
-defDiscrete :: Int -> Discrete Double
-defDiscrete n = discreteDist $ replicate n 1
-
-pdfBenchmarks :: Benchmark
-pdfBenchmarks = bgroup "pdf" [
-  bgroup "normal" [ pdfBenchmark (normalDist 1 5) (4 :: Double),
-                    bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 1 5) . head )) [4 :: Double]
-                  ],
-  bgroup "gamma"  [ pdfBenchmark (gammaDist 2 3) (5 :: Double),
-                    bench "grad" $ whnf (head . grad (toLog . pdf (gammaDist 2 3) . head )) [5 :: Double]
-                  ],
-  bgroup "beta"   [ pdfBenchmark (betaDist 2 4) (0.5 :: Double),
-                    bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 2 4) . head )) [0.5 :: Double]
-                  ],
-  bgroup "uniform" [ pdfBenchmark (uniformDist 7 10) (8 :: Double),
-                    bench "grad" $ whnf (head . grad (toLog . pdf (normalDist 7 10) . head )) [8 :: Double]
-                  ],
-  benchN "discrete" defNs (\n -> pdfBenchmark (defDiscrete n) 0)
-  ]
-
-samplingBenchmarks :: GenIO -> Benchmark
-samplingBenchmarks g = bgroup "sample" [
-  bench "normal"  $ whnfIO $ sampleIOwith (normal 1 5)   g,
-  bench "gamma"   $ whnfIO $ sampleIOwith (gamma 2 3)    g,
-  bench "beta"    $ whnfIO $ sampleIOwith (beta 2 4)     g,
-  bench "uniform" $ whnfIO $ sampleIOwith (uniform 7 10) g,
-  benchN "discrete" defNs (\n -> bench "sample" $ whnfIO $ sampleIOwith (sample $ defDiscrete n) g)
-  ]
-
-
 benchmarkableIS :: NFData a => GenIO -> Weighted SamplerIO a -> Benchmarkable
-benchmarkableIS g m = nfIO $ fmap (second toLog) $ sampleIOwith (runWeighted m) g
+benchmarkableIS g m = nfIO $ second ln <$> sampleIOwith (runWeighted m) g
 
 benchIS :: NFData a => GenIO -> Weighted SamplerIO a -> Benchmark
 benchIS g m = bench "IS" $ benchmarkableIS g m
 
 benchSMC :: NFData a => Int -> [Int] -> GenIO -> Sequential (Population SamplerIO) a -> Benchmark
 benchSMC k ns g m =
-  benchN "SMC" ns $ \n -> bench "" $ nfIO $ fmap (map fst) $ sampleIOwith (runPopulation $ smcMultinomial k n m) g
+  benchN "SMC" ns $ \n -> bench "" $ nfIO $ map fst <$> sampleIOwith (runPopulation $ smcMultinomial k n m) g
 
 benchMH :: NFData a => [Int] -> GenIO -> Traced (Weighted SamplerIO) a -> Benchmark
 benchMH ns g m =
-  benchN "MH" ns $ \n -> bench "" $ nfIO $ sampleIOwith (prior $ marginal $ composeN n (mhStep k) m) g where
-    k = randomWalkKernel 1
+  benchN "MH" ns $ \n -> bench "" $ nfIO $ sampleIOwith (prior $ marginal $ composeN n mhStep m) g where
 
     composeN :: Int -> (a -> a) -> a -> a
     composeN 0 _ x = x
@@ -84,8 +42,7 @@ benchMH ns g m =
 
 benchSMCRM :: NFData a => Int -> [Int] -> [Int] -> GenIO -> Sequential (Traced (Population SamplerIO)) a -> Benchmark
 benchSMCRM k ns ts g m =
-  benchN "SMCRM" ns $ \n -> benchN "Trans" ts $ \t -> bench "" $ nfIO $ sampleIOwith (fmap (map fst) $ runPopulation $ smcRM k n t kernel m) g where
-    kernel = randomWalkKernel 1
+  benchN "SMCRM" ns $ \n -> benchN "Trans" ts $ \t -> bench "" $ nfIO $ sampleIOwith (fmap (map fst) $ runPopulation $ smcRM k n t m) g
 
 type Model a = (Sequential (Population SamplerIO) a, Traced (Weighted SamplerIO) a, Sequential (Traced (Population SamplerIO)) a)
 
@@ -118,9 +75,9 @@ main = do
 
   g <- createSystemRandom
 
-  let lr = (LogReg.logisticRegression, unconstrain LogReg.logisticRegression, unconstrain LogReg.logisticRegression)
+  let lr = (LogReg.logisticRegression, LogReg.logisticRegression, LogReg.logisticRegression)
   let hmm = (HMM.hmm, HMM.hmm, HMM.hmm)
-  let lda = (LDA.lda, unconstrain LDA.lda, unconstrain LDA.lda)
+  let lda = (LDA.lda, LDA.lda, LDA.lda)
 
   let benchmarks = [
         benchModel "LogReg" (length LogReg.xs) g lr,
--- models/LDA.hs
+++ models/LDA.hs
@@ -3,9 +3,11 @@
 
 module LDA where
 
+import Numeric.Log
+import Data.Vector as V hiding (length, zip, mapM, mapM_)
 import qualified Data.Map as Map
 
-import Control.Monad.Bayes.Simple
+import Control.Monad.Bayes.Class
 
 vocabluary :: [String]
 vocabluary = ["bear", "wolf", "python", "prolog"]
@@ -21,29 +23,29 @@ docs = [
   words "bear wolf bear python bear wolf bear wolf bear wolf"
   ]
 
-word_dist_prior :: (MonadBayes m, CustomReal m ~ Double) => m [Double]
-word_dist_prior = dirichlet $ map (const 1) vocabluary
+word_dist_prior :: MonadSample m => m (Vector Double)
+word_dist_prior = dirichlet $ V.replicate (length vocabluary) 1
 
-topic_dist_prior :: (MonadBayes m, CustomReal m ~ Double) => m [Double]
-topic_dist_prior = dirichlet $ map (const 1) topics
+topic_dist_prior :: MonadSample m => m (Vector Double)
+topic_dist_prior = dirichlet $ V.replicate (length topics) 1
 
 word_index :: Map.Map String Int
 word_index = Map.fromList $ zip vocabluary [0..]
 
-lda :: (MonadBayes m, CustomReal m ~ Double) => m [Int]
+lda :: MonadInfer m => m [Int]
 lda = do
   word_dist_for_topic <- do
-    ts <- mapM (const $ fmap discreteDist word_dist_prior) [0 .. length topics]
+    ts <- mapM (const word_dist_prior) [0 .. length topics]
     return $ Map.fromList $ zip [0 .. length topics] ts
 
   let obs doc = do
-        topic_dist <- fmap discreteDist topic_dist_prior
+        topic_dist <- fmap categorical topic_dist_prior
         let f word = do
-              topic <- sample topic_dist
-              observe (word_dist_for_topic Map.! topic) (word_index Map.! word)
+              topic <- topic_dist
+              factor $ (Exp . log) $ (word_dist_for_topic Map.! topic) V.! (word_index Map.! word)
         mapM_ f doc
 
   mapM_ obs docs
 
   -- return samples since Discrete is not NFData
-  mapM (sample . snd) $ Map.toList word_dist_for_topic
+  mapM (categorical . snd) $ Map.toList word_dist_for_topic
