6e2324174dc8bf8f5f2ef635d6d937790914b3d1
Adam Scibior <ams240@cam.ac.uk>
2017-11-08 11:25:10 +0000

    Improve SMC2 implementation

--- src/Control/Monad/Bayes/Inference/SMC2.hs
+++ src/Control/Monad/Bayes/Inference/SMC2.hs
@@ -18,42 +18,18 @@ import Numeric.Log
 import Control.Monad.Trans
 
 import Control.Monad.Bayes.Class
-import Control.Monad.Bayes.Weighted as Weighted
-import Control.Monad.Bayes.Free as Free
 import Control.Monad.Bayes.Population as Pop
-import Control.Monad.Bayes.Sequential as Seq
-import Control.Monad.Bayes.Traced as Tr
+import Control.Monad.Bayes.Inference
 import Control.Monad.Bayes.Helpers
 
--- | Apply a function a given number of times.
-composeCopies :: Int -> (a -> a) -> (a -> a)
-composeCopies k f = foldr (.) id (replicate k f)
-
-latent :: Monad m => S (P (P m)) a -> S (T (P (P m))) a
-latent = Seq.hoist lift
-
-pf :: MonadSample m
-          => Weighted (FreeSampler (Population m)) a
-          -> Weighted (FreeSampler (Population m)) [(a, Log Double)]
-pf =
-  hoistWF lift .
-  runPopulation .
-  pushWeight .
-  hoistW Free.pullPopulation
-
-pfInv :: MonadSample m
-      => Weighted (FreeSampler (Population m)) [(a, Log Double)]
-      -> Weighted (FreeSampler (Population m)) a
-pfInv =
-  hoistW pushPopulation .
-  Pop.pullWeight .
-  fromWeightedList .
-  hoistWF (prior . proper)
+latent :: Monad m => m a -> S (P (S (T (P m)))) a
+latent = lift . lift . lift . lift . lift
 
 smc2 :: MonadSample m
-     => Int -> Int -> Int -> Int -> S (T (P (P m))) a -> P m a
-smc2 k n p t =
-  Pop.flatten .
-  marginal . finish .
-  composeCopies k (advance . hoistSM resample . hoistS (transformModel pfInv . composeCopies t mhStep . transformModel pf) . hoistSTP resample . hoistSM (lift . collapse)) .
-  hoistSTP (spawn n >>) . hoistSM (spawn p >>)
+     => Int -- ^ number of time steps
+     -> Int -- ^ number of inner particles
+     -> Int -- ^ number of outer particles
+     -> Int -- ^ number of MH transitions
+     -> S (P (S (T (P m)))) a -> P m [(a, Log Double)]
+-- TODO: implement smcSystematicPushingEvidence to get true SMC2
+smc2 k n p t = smcRM k p t . runPopulation . smcSystematic k n -- smcSystematicPusingEvidence k n
