260b93d90bff12d1872c065549a30464c705036d
Adam Scibior <ams240@cam.ac.uk>
2017-11-12 19:00:25 +0000

    Benchmark Anglican from Haskell

--- benchmark/Speed.hs
+++ benchmark/Speed.hs
@@ -2,6 +2,11 @@ import Criterion.Main
 import Criterion.Types
 import System.Random.MWC (createSystemRandom, GenIO)
 
+import GHC.IO.Handle
+import System.Exit
+import System.Process hiding (env)
+import Control.Monad.Par.Class (NFData)
+
 import Control.Monad.Bayes.Class
 import Control.Monad.Bayes.Sampler
 import Control.Monad.Bayes.Weighted
@@ -15,6 +20,37 @@ import qualified HMM
 import qualified LogReg
 import qualified LDA
 
+-- | Path to the Anglican project with benchmarks.
+anglicanPath :: String
+anglicanPath = "/scratch/ams240/repos/anglican-white-paper/experiments"
+
+-- | Running Leiningen repl process.
+-- Leiningen takes a lot of time to start so we keep a repl
+-- running to speed up benchmarking.
+-- Contains in order input handle, output handle, and process handle.
+data LeinProc = LeinProc Handle Handle ProcessHandle
+
+-- | Wait until an Anglican program finishes
+waitForAnglican :: Handle -> IO ()
+waitForAnglican handle = run where
+  run = do
+    l <- hGetLine handle
+    if l == "nil" then
+      return ()
+    else
+      run
+
+-- | Start a Leiningen process in a directory that contains benchmarks.
+startLein :: IO LeinProc
+startLein = do
+  let setup = (shell "lein repl"){cwd = Just anglicanPath, std_in = CreatePipe, std_out = CreatePipe}
+  (Just input, Just output, _, process) <- createProcess setup
+  -- wait until Leiningen starts producing output to make sure it's ready
+  hWaitForInput output (-1) -- wait for output indefinitely
+  return $ LeinProc input output process
+
+-- | Environment to execute benchmarks in.
+data Env = Env {rng :: GenIO, lein :: LeinProc}
 
 data ProbProgSys = MonadBayes | Anglican | WebPPL
   deriving(Show)
@@ -48,16 +84,25 @@ prepareBenchmarkable g MonadBayes model alg = nfIO $ sampleIOwith (runAlg model 
 prepareBenchmarkable _ Anglican _ _  = error "Anglican benchmarks not available"
 prepareBenchmarkable _ WebPPL _ _ = error "WebPPL benchmarks not available"
 
-prepareBenchmark :: GenIO -> ProbProgSys -> Model -> Alg -> Benchmark
-prepareBenchmark g system model alg =
-  bench (show system ++ sep ++ show model ++ sep ++ show alg) $
-  prepareBenchmarkable g system model alg where
+prepareBenchmark :: Env -> ProbProgSys -> Model -> Alg -> Benchmark
+prepareBenchmark e MonadBayes model alg =
+  bench (show MonadBayes ++ sep ++ show model ++ sep ++ show alg) $
+  prepareBenchmarkable (rng e) MonadBayes model alg where
     sep = "_"
+prepareBenchmark e Anglican model alg = bench "Anglican" $ whnfIO collect where
+  (LeinProc input output process) = (lein e)
+  collect = do
+    let cmdString = "(m! lda -a smc -n 1000)\n"
+    hPutStr input cmdString
+    hFlush input
+    waitForAnglican output
 
 main :: IO ()
 main = do
 
   g <- createSystemRandom
+  l <- startLein
+  let e = Env g l
 
   let systems = [MonadBayes]
 
@@ -74,13 +119,15 @@ main = do
 
   let algs = [MH 10, SMC 10, RMSMC 10 1]
 
-  let benchmarks = map (uncurry3 (prepareBenchmark g)) xs where
-        uncurry3 f (x,y,z) = f x y z
-        xs = do
-          s <- systems
-          m <- models
-          a <- algs
-          return (s,m,a)
+  -- let benchmarks = map (uncurry3 (prepareBenchmark e)) xs where
+  --       uncurry3 f (x,y,z) = f x y z
+  --       xs = do
+  --         s <- systems
+  --         m <- models
+  --         a <- algs
+  --         return (s,m,a)
+  let benchmarks = [prepareBenchmark e Anglican undefined undefined]
 
-  let config = defaultConfig{csvFile = Just "speed.csv"}
+  -- let config = defaultConfig{csvFile = Just "speed.csv"}
+  let config = defaultConfig
   defaultMainWith config benchmarks
