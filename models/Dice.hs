8eecadcb0297cae2c124d06922823b0b1641adc2
Vincent Hui <vincenthk007@gmail.com>
2018-09-26 10:14:51 +0800

    fix Dice model

--- models/Dice.hs
+++ models/Dice.hs
@@ -6,29 +6,27 @@ module Dice where
 -- Exact results can be obtained using Dist monad
 
 import Control.Monad (liftM2)
-
 import Control.Monad.Bayes.Class
-import Control.Monad.Bayes.Simple
 
 -- | A toss of a six-sided die.
-die :: MonadDist d => d Int
-die = categorical $ zip [1..6] (repeat (1/6))
+die :: MonadSample m => m Int
+die = uniformD [1..6]
 
 -- | A sum of outcomes of n independent tosses of six-sided dice.
-dice :: MonadDist d => Int -> d Int
+dice :: MonadSample m => Int -> m Int
 dice 1 = die
 dice n = liftM2 (+) die (dice (n-1))
 
 -- | Toss of two dice where the output is greater than 4.
-dice_hard :: MonadBayes d => d Int
+dice_hard :: MonadInfer m => m Int
 dice_hard = do
   result <- dice 2
   condition (result > 4)
   return result
 
 -- | Toss of two dice with an artificial soft constraint.
-dice_soft :: MonadBayes d => d Int
+dice_soft :: MonadInfer m => m Int
 dice_soft = do
   result <- dice 2
-  factor (1 / fromIntegral result)
+  score (1 / fromIntegral result)
   return result
