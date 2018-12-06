8f1368203433d9a60a7768a4f193e1a3579dcc23
Eli Sennesh <esennesh@ccis.neu.edu>
2018-12-06 16:55:44 -0500

    Control.Monad.Bayes.Weighted: scoreImportance with proposal and prior PDF
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- src/Control/Monad/Bayes/Weighted.hs
+++ src/Control/Monad/Bayes/Weighted.hs
@@ -20,6 +20,7 @@ module Control.Monad.Bayes.Weighted (
     flatten,
     applyWeight,
     hoist,
+    scoreImportance,
                   ) where
 
 import Control.Monad.Trans
@@ -69,6 +70,18 @@ applyWeight :: MonadCond m => Weighted m a -> m a
 applyWeight m = do
   (x, w) <- runWeighted m
   factor w
+  return x
+
+propose :: MonadCond m => Weighted m a -> Weighted m a
+propose m = do
+  x <- m
+  Weighted $ modify (\w -> w**(-2))
+  return x
+
+scoreImportance :: MonadCond m => Weighted m a -> (a -> Log Double) -> Weighted m a
+scoreImportance proposal prior = do
+  x <- propose proposal
+  score (prior x)
   return x
 
 -- | Apply a transformation to the transformed monad.
