a7987432248b5d2e1191fce5641d60a476e1c45a
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-03-03 14:13:56 +0100

    (build) use nixos-20.03, lts-15.1

--- nix/update.nix
+++ nix/update.nix
@@ -11,7 +11,7 @@ writeScript "update.sh" ''
 
   nixprefetchgit="${nix-prefetch-git}/bin/nix-prefetch-git"
 
-  channel='nixos-unstable'
+  channel='nixos-20.03'
   nix-prefetch-git https://github.com/nixos/nixpkgs-channels.git --rev "refs/heads/$channel" > ./nix/nixpkgs.json
   nix-prefetch-git https://github.com/input-output-hk/haskell.nix.git --rev "refs/heads/master" > ./nix/haskell-nix.json
 ''
