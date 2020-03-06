641fca6f87c209b04b703b97568e29a32eeb7fa1
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-03-06 15:08:10 +0100

    (build) shellcheck (#85)

--- nix/lint.nix
+++ nix/lint.nix
@@ -6,6 +6,7 @@
 
 writeScript "lint.sh" ''
   #!/usr/bin/env ${pkgs.bash}/bin/bash
+  # shellcheck shell=bash
 
   set -euo pipefail
   IFS=$'\n\t'
@@ -16,6 +17,9 @@ writeScript "lint.sh" ''
   nixpkgsfmt="${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"
   ormolu="${pkgs.ormolu}/bin/ormolu"
   prettier="${pkgs.nodePackages.prettier}/bin/prettier"
+  shellcheck="${pkgs.shellcheck}/bin/shellcheck"
+
+  fixmsg="Run 'eval \$(nix-build . -A fix)' to fix this"
 
   if ! $git diff --quiet; then
       echo 'ERROR: Dirty working directory'
@@ -25,7 +29,7 @@ writeScript "lint.sh" ''
   $git ls-tree -z -r HEAD --name-only | grep -z '\.cabal$' | xargs -0 $cabal format
   if ! $git diff --exit-code; then
       echo 'FAILURE: Cabal files were not formatted correctly'
-      echo 'Run "eval $(nix-build -A fix)" to fix this'
+      echo "$fixmsg"
       exit 1
   fi
   echo 'SUCCESS: Cabal files are formatted correctly'
@@ -33,17 +37,30 @@ writeScript "lint.sh" ''
   $git ls-tree -z -r HEAD --name-only | grep -z '\.nix$' | xargs -0 $nixpkgsfmt
   if ! $git diff --exit-code; then
       echo 'FAILURE: Nix files were not formatted correctly'
-      echo 'Run "eval $(nix-build -A fix)" to fix this'
+      echo "$fixmsg"
       exit 1
   fi
   echo 'SUCCESS: Nix files are formatted correctly'
 
   if ! $git ls-tree -z -r HEAD --name-only | grep -z '\.\(yaml\|yml\|json\)$' | xargs -0 $prettier --list-different; then
       echo 'FAILURE: YAML and JSON files were not formatted correctly'
-      echo 'Run "eval $(nix-build -A fix)" to fix this'
+      echo "$fixmsg"
       exit 1
   fi
   echo 'SUCCESS: YAML and JSON files are formatted correctly'
+
+  # List all generated Bash scripts here.
+  bash_files=(
+    "$(nix-build . -A lint)"
+    "$(nix-build . -A fix)"
+    "$(nix-build . -A update)"
+  )
+  if ! $shellcheck "$($git ls-tree -r HEAD --name-only | grep '\.sh')" "''${bash_files[@]}"; then
+      echo 'FAILURE: Bash files failed checks'
+      exit 1
+  fi
+  echo 'SUCCESS: Bash files passed checks'
+
 
   function haskell_files {
       $git ls-tree -z -r HEAD --name-only | grep -z '\.hs$'
@@ -51,14 +68,15 @@ writeScript "lint.sh" ''
 
   if ! haskell_files | xargs -0 $hlint; then
       echo 'FAILURE: HLint reported issues'
-      echo 'Run "eval $(nix-build -A fix)" to attempt to fix this, manual fixes may be necessary'
+      echo "$fixmsg"
+      echo 'Manual fixes may be necessary'
       exit 1
   fi
   echo 'SUCCESS: HLint reported no issues'
 
   if ! haskell_files | xargs -0 -I{} $ormolu --mode check {}; then
       echo 'FAILURE: Haskell files are not formatted correctly'
-      echo 'Run "eval $(nix-build -A fix)" to fix this'
+      echo "$fixmsg"
       exit 1
   fi
   echo 'SUCCESS: Haskell files are formatted correctly'
--- nix/update.nix
+++ nix/update.nix
@@ -5,6 +5,7 @@
 
 writeScript "update.sh" ''
   #!/usr/bin/env ${bash}/bin/bash
+  # shellcheck shell=bash
 
   set -euo pipefail
   IFS=$'\n\t'
@@ -12,6 +13,6 @@ writeScript "update.sh" ''
   nixprefetchgit="${nix-prefetch-git}/bin/nix-prefetch-git"
 
   channel='nixos-20.03'
-  nix-prefetch-git https://github.com/nixos/nixpkgs-channels.git --rev "refs/heads/$channel" > ./nix/nixpkgs.json
-  nix-prefetch-git https://github.com/input-output-hk/haskell.nix.git --rev "refs/heads/master" > ./nix/haskell-nix.json
+  $nixprefetchgit https://github.com/nixos/nixpkgs-channels.git --rev "refs/heads/$channel" > ./nix/nixpkgs.json
+  $nixprefetchgit https://github.com/input-output-hk/haskell.nix.git --rev "refs/heads/master" > ./nix/haskell-nix.json
 ''
