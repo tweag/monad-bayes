0cfd16365e6b481f74ce0d1307a8a3e14541da62
curiousleo <curiousleo@users.noreply.github.com>
2021-04-22 17:08:17 +0200

    nixpkgs-fmt

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,10 +1,12 @@
 let
   haskellNixSrc = builtins.fromJSON (builtins.readFile ./haskell-nix.json);
-  haskellNix = import (
-    builtins.fetchTarball {
-      url = "https://github.com/input-output-hk/haskell.nix/archive/${haskellNixSrc.rev}.tar.gz";
-      inherit (haskellNixSrc) sha256;
-    }
-  ) {};
+  haskellNix = import
+    (
+      builtins.fetchTarball {
+        url = "https://github.com/input-output-hk/haskell.nix/archive/${haskellNixSrc.rev}.tar.gz";
+        inherit (haskellNixSrc) sha256;
+      }
+    )
+    { };
 in
 import haskellNix.sources.nixpkgs-2009 haskellNix.nixpkgsArgs
