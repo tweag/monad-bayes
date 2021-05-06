4ea10b140f3a689c4bbcf9708ae7554faa4449da
Dominic Steinitz <dominic@steinitz.org>
2021-05-06 12:14:53 +0100

    Revert "Experiment"
    This reverts commit 9fedb153103445ccd652417084289c3d1ca7b2d1.

--- src/Control/Monad/Bayes/Sampler.hs
+++ src/Control/Monad/Bayes/Sampler.hs
@@ -1,10 +1,6 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-
 
 -- |
 -- Module      : Control.Monad.Bayes.Sampler
@@ -36,29 +32,10 @@ import Control.Monad.State (State, state)
 import Control.Monad.Trans (MonadIO, lift)
 import Control.Monad.Trans.Reader (ReaderT, ask, mapReaderT, runReaderT)
 import System.Random.MWC
-import qualified Data.Random as DR
-import qualified Data.Random.Distribution.Beta as DR
-import qualified Data.Random.Distribution.Bernoulli as DR
-import qualified Data.Random.Distribution.Categorical as DR
-import qualified Data.Vector as V
-import qualified Data.Vector.Generic as VG
 import qualified System.Random.MWC.Distributions as MWC
-import Control.Monad.Primitive
-import qualified System.Random as R
-import qualified System.Random.Stateful as RS
-import qualified Control.Monad.Reader as MR
-
-
-
 
 -- | An 'IO' based random sampler using the MWC-Random package.
 newtype SamplerIO a = SamplerIO (ReaderT GenIO IO a)
-  deriving newtype (Functor, Applicative, Monad, MonadIO)
-
-newtype SamplerIO' a = SamplerIO' (ReaderT (RS.IOGenM RS.StdGen) IO a)
-  deriving newtype (Functor, Applicative, Monad, MonadIO)
-
-newtype SamplerIO'' a = SamplerIO'' (ReaderT (RS.STGenM RS.StdGen RealWorld) IO a)
   deriving newtype (Functor, Applicative, Monad, MonadIO)
 
 -- | Initialize a pseudo-random number generator using randomness supplied by
@@ -73,9 +50,6 @@ sampleIO (SamplerIO m) = createSystemRandom >>= runReaderT m
 sampleIOfixed :: SamplerIO a -> IO a
 sampleIOfixed (SamplerIO m) = create >>= runReaderT m
 
-sampleIOfixed' :: SamplerIO' a -> IO a
-sampleIOfixed' (SamplerIO' m) = (RS.newIOGenM (RS.mkStdGen 42)) >>= runReaderT m
-
 -- | Like 'sampleIO' but with a custom pseudo-random number generator.
 sampleIOwith :: SamplerIO a -> GenIO -> IO a
 sampleIOwith (SamplerIO m) = runReaderT m
@@ -83,45 +57,24 @@ sampleIOwith (SamplerIO m) = runReaderT m
 fromSamplerST :: SamplerST a -> SamplerIO a
 fromSamplerST (SamplerST m) = SamplerIO $ mapReaderT stToIO m
 
-fromSamplerST'' :: SamplerST' a -> SamplerIO'' a
-fromSamplerST'' (SamplerST' m) = SamplerIO'' $ mapReaderT stToIO m
-
 instance MonadSample SamplerIO where
   random = fromSamplerST random
-
--- instance MonadSample SamplerIO' where
---   random = fromSamplerST' random
 
 -- | An 'ST' based random sampler using the @mwc-random@ package.
 newtype SamplerST a = SamplerST (forall s. ReaderT (GenST s) (ST s) a)
 
-newtype SamplerST' a = SamplerST' (forall s . ReaderT (RS.STGenM RS.StdGen s) (ST s) a)
-
 runSamplerST :: SamplerST a -> ReaderT (GenST s) (ST s) a
 runSamplerST (SamplerST s) = s
 
-runSamplerST' :: SamplerST' a -> ReaderT (RS.STGenM RS.StdGen s) (ST s) a
-runSamplerST' (SamplerST' s) = s
-
 instance Functor SamplerST where
   fmap f (SamplerST s) = SamplerST $ fmap f s
-
-instance Functor SamplerST' where
-  fmap f (SamplerST' s) = SamplerST' $ fmap f s
 
 instance Applicative SamplerST where
   pure x = SamplerST $ pure x
   (SamplerST f) <*> (SamplerST x) = SamplerST $ f <*> x
 
-instance Applicative SamplerST' where
-  pure x = SamplerST' $ pure x
-  (SamplerST' f) <*> (SamplerST' x) = SamplerST' $ f <*> x
-
 instance Monad SamplerST where
   (SamplerST x) >>= f = SamplerST $ x >>= runSamplerST . f
-
-instance Monad SamplerST' where
-  (SamplerST' x) >>= f = SamplerST' $ x >>= runSamplerST' . f
 
 -- | Run the sampler with a supplied seed.
 -- Note that 'State Seed' is much less efficient than 'SamplerST' for composing computation.
@@ -139,32 +92,9 @@ sampleSTfixed (SamplerST s) = runST $ do
   gen <- create
   runReaderT s gen
 
-sampleSTfixed' :: SamplerST' a -> a
-sampleSTfixed' (SamplerST' s) = runST $ do
-  gen <- RS.newSTGenM (RS.mkStdGen 42)
-  runReaderT s gen
-
 -- | Convert a distribution supplied by @mwc-random@.
 fromMWC :: (forall s. GenST s -> ST s a) -> SamplerST a
 fromMWC s = SamplerST $ ask >>= lift . s
-
-fromMWC' :: (forall s . (RS.STGenM RS.StdGen s) -> ST s a) -> SamplerST' a
-fromMWC' s = SamplerST' $ ask >>= lift . s
-
--- FIXME: standard deviation or variance?
--- FIXME: shape and scale in the right order?
--- FIXME: beta
-instance MonadSample SamplerST' where
-  random = fromMWC' $ RS.uniformDoublePositive01M
-
-  uniform a b       = fromMWC' $ \g -> DR.sampleFrom g (DR.uniform a b)
-  normal m s        = fromMWC' $ \g -> DR.sampleFrom g (DR.normal m s)
-  gamma shape scale = fromMWC' $ \g -> DR.sampleFrom g (DR.gamma shape scale)
-  beta a b          = fromMWC' $ \g -> DR.sampleFrom g (DR.beta a b)
-
-  bernoulli p    = fromMWC' $ \g -> DR.sampleFrom g (DR.bernoulli p)
-  categorical ps = fromMWC' $ \g -> DR.sampleFrom g (DR.categorical (zip (VG.toList ps) [0 ..]))
-  geometric p    = error "Geometric not implemented in random-fu :("
 
 instance MonadSample SamplerST where
   random = fromMWC System.Random.MWC.uniform
