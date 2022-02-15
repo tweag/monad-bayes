06c63b9724792677dddc8fc24006315ffc8db07d
puretw <richard.wallace@tweag.io>
2022-02-15 09:58:20 -0700

    try something different

--- src/Control/Monad/Bayes/Sampler.hs
+++ src/Control/Monad/Bayes/Sampler.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall #-}
@@ -18,16 +23,16 @@
 -- 'SamplerIO' and 'SamplerST' are instances of 'MonadSample'. Apply a 'MonadCond'
 -- transformer to obtain a 'MonadInfer' that can execute probabilistic models.
 module Control.Monad.Bayes.Sampler
-  ( SamplerIO,
-    sampleIO,
-    sampleIOStdGen,
-    sampleIOfixed,
-    sampleIOwith,
-    -- Seed,
-    SamplerST (SamplerST),
-    runSamplerST,
-    sampleSTfixed,
-  )
+  -- ( SamplerIO,
+  --   sampleIO,
+  --   sampleIOStdGen,
+  --   sampleIOfixed,
+  --   sampleIOwith,
+  --   -- Seed,
+  --   SamplerST (SamplerST),
+  --   runSamplerST,
+  --   sampleSTfixed,
+  -- )
 where
 
 import Control.Monad.Bayes.Class
@@ -41,14 +46,60 @@ import qualified Data.Random.Distribution.Bernoulli as DR
 import qualified Data.Random.Distribution.Beta as DR
 import qualified Data.Random.Distribution.Categorical as DR
 import qualified Data.Vector.Generic as VG
+import GHC.Word
 import qualified System.Random.Stateful as RS
 
--- | An 'IO' based random sampler using the @random-fu@ package.
-newtype SamplerIO g a = SamplerIO (ReaderT g IO a)
-  deriving newtype (Functor, Applicative, Monad, MonadIO)
+import qualified System.Random.MWC as MWC
 
-sampleIO :: SamplerIO g a -> g -> IO a
-sampleIO m = sampleIOwith m
+newtype Sampler g a = Sampler { unSampler :: forall m. RS.StatefulGen g m => ReaderT g m a }
+
+instance Functor (Sampler g) where
+  fmap f (Sampler x) = Sampler (fmap f x)
+
+instance Applicative (Sampler g) where
+  pure x = Sampler $ pure x
+  (Sampler f) <*> (Sampler x) = Sampler (f <*> x)
+
+instance Monad (Sampler g) where
+  return = pure
+  (Sampler x) >>= f = Sampler (x >>= \y -> unSampler $ f y)
+
+
+sampler :: (forall g m. RS.StatefulGen g m => g -> m a) -> Sampler g a
+sampler f = Sampler (ask >>= lift . f)
+
+instance (RS.StatefulGen g (ST RealWorld)) => MonadSample (Sampler g) where
+  random = sampler RS.uniformDoublePositive01M
+
+  uniform a b = sampler $ \g -> DR.sampleFrom g (DR.uniform a b)
+  normal m s = sampler $ \g -> DR.sampleFrom g (DR.normal m s)
+  gamma shape scale = sampler $ \g -> DR.sampleFrom g (DR.gamma shape scale)
+  beta a b = sampler $ \g -> DR.sampleFrom g (DR.beta a b)
+
+  bernoulli p = sampler $ \g -> DR.sampleFrom g (DR.bernoulli p)
+  categorical ps = sampler $ \g -> DR.sampleFrom g (DR.categorical (zip (VG.toList ps) [0 ..]))
+  geometric _p = error "Geometric not implemented in random-fu :("
+
+runSampler :: RS.StatefulGen g m => Sampler g a -> g -> m a
+runSampler (Sampler m) = runReaderT m
+
+runSamplerIO :: RS.StatefulGen g IO => Sampler g a -> g -> IO a
+runSamplerIO = runSampler
+
+runSamplerST :: RS.StatefulGen g (ST s) => Sampler g a -> g -> ST s a
+runSamplerST = runSampler
+
+foo1 :: PrimMonad m => Sampler (MWC.Gen (PrimState m)) b -> m b
+foo1 m = do
+  g <- MWC.create
+  runSampler m g
+
+bar1 :: Sampler (RS.STGenM RS.StdGen s) b -> ST s b
+bar1 m = do
+  g <- RS.newSTGenM (RS.mkStdGen 42)
+  runSampler m g
+
+newtype SamplerIO g a = SamplerIO (RS.StatefulGen g IO => Sampler g a)
 
 -- | Initialize a pseudo-random number generator using randomness supplied by
 -- the operating system.
@@ -56,61 +107,57 @@ sampleIO m = sampleIOwith m
 -- once per program.
 
 -- FIXME: A lie
-sampleIOStdGen :: SamplerIO (RS.STGenM RS.StdGen RealWorld) a -> IO a
-sampleIOStdGen m = sampleIO m =<< stToIO (RS.newSTGenM (RS.mkStdGen 42))
+-- sampleIOStdGen :: SamplerIO (RS.STGenM RS.StdGen RealWorld) a -> IO a
+-- sampleIOStdGen (SamplerIO m) = stToIO $ runSampler m =<< RS.newSTGenM (RS.mkStdGen 42)
 
--- | Like 'sampleIO', but with a fixed random seed.
--- Useful for reproducibility.
-sampleIOfixed :: SamplerIO (RS.STGenM RS.StdGen RealWorld) a -> IO a
-sampleIOfixed m = sampleIO m =<< stToIO (RS.newSTGenM (RS.mkStdGen 42))
 
--- | Like 'sampleIO' but with a custom pseudo-random number generator.
-sampleIOwith :: SamplerIO g a -> g -> IO a
-sampleIOwith (SamplerIO m) = runReaderT m
+newtype SamplerST g a = SamplerST (forall s. RS.StatefulGen g (ST s) => Sampler g a)
 
-fromSamplerST :: RS.StatefulGen g (ST RealWorld) => SamplerST a -> SamplerIO g a
-fromSamplerST (SamplerST m) = SamplerIO $ mapReaderT stToIO m
+-- -- | Like 'sampleIO', but with a fixed random seed.
+-- -- Useful for reproducibility.
+-- sampleIOFixed :: Sampler (RS.STGenM RS.StdGen RealWorld) a -> IO a
+-- sampleIOFixed m = stToIO $ sample m =<< RS.newSTGenM (RS.mkStdGen 42)
 
-instance RS.StatefulGen g (ST RealWorld) => MonadSample (SamplerIO g) where
-  random = fromSamplerST random
+-- -- fromSamplerST :: (RS.StatefulGen g (ST RealWorld), RS.StatefulGen g' IO) => Sampler g a -> Sampler g' a
+-- -- fromSamplerST (Sampler m) = Sampler $ mapReaderT stToIO m
 
--- | An 'ST' based random sampler using the @random-fu@ package.
-newtype SamplerST a = SamplerST (forall s g. RS.StatefulGen g (ST s) => ReaderT g (ST s) a)
+-- -- | An 'ST' based random sampler using the @random-fu@ package.
+-- newtype SamplerST a = SamplerST (forall s g. RS.StatefulGen g (ST s) => ReaderT g (ST s) a)
 
-runSamplerST :: RS.StatefulGen g (ST s) => SamplerST a -> ReaderT g (ST s) a
-runSamplerST (SamplerST s) = s
+-- runSamplerST :: RS.StatefulGen g (ST s) => SamplerST a -> ReaderT g (ST s) a
+-- runSamplerST (SamplerST s) = s
 
-instance Functor SamplerST where
-  fmap f (SamplerST s) = SamplerST $ fmap f s
+-- instance Functor SamplerST where
+--   fmap f (SamplerST s) = SamplerST $ fmap f s
 
-instance Applicative SamplerST where
-  pure x = SamplerST $ pure x
-  (SamplerST f) <*> (SamplerST x) = SamplerST $ f <*> x
+-- instance Applicative SamplerST where
+--   pure x = SamplerST $ pure x
+--   (SamplerST f) <*> (SamplerST x) = SamplerST $ f <*> x
 
-instance Monad SamplerST where
-  (SamplerST x) >>= f = SamplerST $ x >>= runSamplerST . f
+-- instance Monad SamplerST where
+--   (SamplerST x) >>= f = SamplerST $ x >>= runSamplerST . f
 
--- | Run the sampler with a fixed random seed.
-sampleSTfixed :: SamplerST a -> a
-sampleSTfixed (SamplerST s) = runST $ do
-  gen <- RS.newSTGenM (RS.mkStdGen 42)
-  runReaderT s gen
+-- -- | Run the sampler with a fixed random seed.
+-- sampleSTfixed :: SamplerST a -> a
+-- sampleSTfixed (SamplerST s) = runST $ do
+--   gen <- RS.newSTGenM (RS.mkStdGen 42)
+--   runReaderT s gen
 
--- | Convert a distribution supplied by @random-fu@.
-fromRandomFu :: (forall s g. RS.StatefulGen g (ST s) => g -> ST s a) -> SamplerST a
-fromRandomFu s = SamplerST $ ask >>= lift . s
+-- -- | Convert a distribution supplied by @random-fu@.
+-- fromRandomFu :: (forall s g. RS.StatefulGen g (ST s) => g -> ST s a) -> SamplerST a
+-- fromRandomFu s = SamplerST $ ask >>= lift . s
 
--- FIXME: standard deviation or variance?
--- FIXME: shape and scale in the right order?
--- FIXME: beta
-instance MonadSample SamplerST where
-  random = fromRandomFu RS.uniformDoublePositive01M
+-- -- FIXME: standard deviation or variance?
+-- -- FIXME: shape and scale in the right order?
+-- -- FIXME: beta
+-- instance MonadSample SamplerST where
+--   random = fromRandomFu RS.uniformDoublePositive01M
 
-  uniform a b = fromRandomFu $ \g -> DR.sampleFrom g (DR.uniform a b)
-  normal m s = fromRandomFu $ \g -> DR.sampleFrom g (DR.normal m s)
-  gamma shape scale = fromRandomFu $ \g -> DR.sampleFrom g (DR.gamma shape scale)
-  beta a b = fromRandomFu $ \g -> DR.sampleFrom g (DR.beta a b)
+--   uniform a b = fromRandomFu $ \g -> DR.sampleFrom g (DR.uniform a b)
+--   normal m s = fromRandomFu $ \g -> DR.sampleFrom g (DR.normal m s)
+--   gamma shape scale = fromRandomFu $ \g -> DR.sampleFrom g (DR.gamma shape scale)
+--   beta a b = fromRandomFu $ \g -> DR.sampleFrom g (DR.beta a b)
 
-  bernoulli p = fromRandomFu $ \g -> DR.sampleFrom g (DR.bernoulli p)
-  categorical ps = fromRandomFu $ \g -> DR.sampleFrom g (DR.categorical (zip (VG.toList ps) [0 ..]))
-  geometric _p = error "Geometric not implemented in random-fu :("
+--   bernoulli p = fromRandomFu $ \g -> DR.sampleFrom g (DR.bernoulli p)
+--   categorical ps = fromRandomFu $ \g -> DR.sampleFrom g (DR.categorical (zip (VG.toList ps) [0 ..]))
+--   geometric _p = error "Geometric not implemented in random-fu :("
