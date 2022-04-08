42a8a408c162179a0d037e1b5dae57088e0072b9
Reuben Cohn-Gordon <reubenharry@gmail.com>
2022-04-08 09:08:26 -0400

    cleanup

--- src/Control/Monad/Bayes/Analytic.hs
+++ src/Control/Monad/Bayes/Analytic.hs
@@ -1,10 +1,18 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE BlockArguments #-}
 
+-- |
+-- This is heavily inspired by https://jtobin.io/giry-monad-implementation
+-- but brought into the monad-bayes framework (i.e. Measure is an inference transformer)
+-- It's largely for debugging other inference methods and didactic use, 
+-- because brute force integration of measures is 
+-- only practical for small programs
+
+
 module Control.Monad.Bayes.Analytic where
 import Control.Monad.Trans.Cont
     ( cont, runCont, Cont, ContT(ContT) )
-import Control.Monad.Bayes.Class (MonadSample (random, bernoulli, normal), condition)
+import Control.Monad.Bayes.Class (MonadSample (random, bernoulli, normal, uniformD), condition)
 import Statistics.Distribution (density)
 import Numeric.Integration.TanhSinh
     ( trap, Result(result), absolute )
@@ -12,13 +20,9 @@ import Control.Monad.Bayes.Weighted (runWeighted, Weighted)
 import qualified Statistics.Distribution.Uniform as Statistics
 import Numeric.Log (Log(ln))
 import Data.Set (Set, fromList, elems)
+import qualified Control.Monad.Bayes.Enumerator
+import Control.Monad (replicateM)
 
--- This is heavily inspired by https://jtobin.io/giry-monad-implementation
--- but brought into the monad-bayes framework (i.e. Measure is an inference transformer)
-
-
--- TODO: bring in the empirical thing from the above articule
--- also try the continuation monad transformer 
 
 newtype Measure a = Measure (Cont Double a) deriving (Functor, Applicative, Monad)
 
@@ -38,24 +42,34 @@ fromDensityFunction d = Measure $ cont $ \f ->
 probability :: Ord a => (a, a) -> Weighted Measure a -> Double
 probability (lower, upper) = runMeasure (\(x,d) -> if x<upper && x  > lower then exp $ ln d else 0) . runWeighted
 
+
+enumerate :: Eq a => Set a -> Weighted Measure a -> [(a, Double)]
+enumerate ls meas = [(val, runMeasure (\(x,d) -> if x == val then exp $ ln d else 0) (runWeighted meas)) | val <- elems ls]
+
+
+
+
 example :: Double
 example = probability (0.8, 2.1) do
     x <- random
     condition (x > 0.7)
     return (x*2)
 
-example2 :: Double
-example2 = runMeasure (\(x,d) -> if x then exp $ ln d else 0) $ runWeighted do
+-- example2 :: Double
+example2 = enumerate (fromList [True, False]) do
     x <- bernoulli 0.7
     condition $ not x
     return x
 
-enumerate :: Eq a => Set a -> Weighted Measure a -> [(a, Double)]
-enumerate ls meas = [(val, runMeasure (\(x,d) -> if x == val then exp $ ln d else 0) (runWeighted meas)) | val <- elems ls]
-
 example3 = enumerate (fromList [True, False]) $ do
 
-    x <- normal 0 1
+    x <- normal 0 1 
     y <- bernoulli x
     condition (not y)
     return (x > 0)
+
+example4 = enumerate (fromList [True, False]) $ do
+
+    x <- uniformD [0 :: Int,1]
+    return (x > 0)
+
--- src/Control/Monad/Bayes/Inference/SMC.hs
+++ src/Control/Monad/Bayes/Inference/SMC.hs
@@ -14,16 +14,15 @@
 -- Arnaud Doucet and Adam M. Johansen. 2011. A tutorial on particle filtering and smoothing: fifteen years later. In /The Oxford Handbook of Nonlinear Filtering/, Dan Crisan and Boris Rozovskii (Eds.). Oxford University Press, Chapter 8.
 module Control.Monad.Bayes.Inference.SMC where
 
-import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Class ( MonadInfer, MonadSample )
 import Control.Monad.Bayes.Population
+    ( Population,
+      spawn,
+      resampleSystematic,
+      resampleMultinomial,
+      pushEvidence )
 import Control.Monad.Bayes.Sequential as Seq
-import Control.Monad.Bayes.Weighted (runWeighted)
-import Numeric.Log (Log)
-import Control.Monad.Bayes.Sampler (sampleIO)
-import Debug.Trace (trace)
-import Control.Monad (void)
-import Control.Monad.Bayes.Enumerator (enumerate, Enumerator, logExplicit)
-import Control.Monad.List (ListT (ListT, runListT))
+    ( Sequential, hoistFirst, sis )
 
 -- | Sequential importance resampling.
 -- Basically an SMC template that takes a custom resampler.
@@ -94,31 +93,3 @@ smcSystematicPush ::
   Population m a
 smcSystematicPush = sir (pushEvidence . resampleSystematic)
 
-model :: MonadInfer m => m Bool
-model = do
-  x <- uniformD [True, False]
-  factor (if x  then 1.0 else 0.5)
-  -- factor (if x  then 1.0 else 0.5)
-  -- condition x
-  return x -- $ trace (show (x, "trace")) x
-
--- ex1 :: MonadInfer m => m
---   ([(Bool, Log Double)], Log Double)
--- ex1 :: [([(Bool, Log Double)], Double)]
-ex1 = logExplicit $ runPopulation $ finish $ composeCopies 1 (advance . Seq.hoistFirst resampleMultinomial) $ Seq.hoistFirst (spawn 2 >>) model
-
-ex2 = logExplicit $ runPopulation $ finish $ composeCopies 1 (advance . Seq.hoistFirst resampleMultinomial . advance) $ Seq.hoistFirst (spawn 2 >>) model
-ex3 = enumerate $ runPopulation $ (spawn 2 >> model)
-
-ex5 = logExplicit $ sis (uniformD [(), ()] >>) 1 $ advance $ advance $ model
-
-ex6 = logExplicit (score 0.00001 >> uniformD [True,False])
-
-composeCopies k f = foldr (.) id (replicate k f)
-
-
--- ex4 :: ListT Enumerator Bool
--- ex4 = enumerate $ runListT $ do
---   x <- uniformD [True, False]
---   condition x
---   return x
