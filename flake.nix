8540805cf7437cf39198381b1853826643d019f2
idontgetoutmuch <dominic@steinitz.org>
2022-08-30 14:45:42 +0100

    Lazyppl (#178)
    * analytic
    
    * docs
    
    * docs
    
    * docs
    
    * docs
    
    * inference
    
    * executable docs
    
    * more comments, added BayesianModel
    
    * Remove MaybeT and use ExceptT
    
    * remove vscode and update gitignore
    
    * delete additional changes: Failure
    
    * delete broken files
    
    * update test for enumerator
    
    * update models
    
    * tips on inference
    
    * density
    
    * analytic
    
    * analytic
    
    * more pipes
    
    * update docs
    
    * linear outliers as in Gen tutorial
    
    * update docs
    
    * cleanup SMC.hs
    
    * more cleanup, and explicit imports
    
    * remove Rejection
    
    * cleanup
    
    * enumerate
    
    * binning
    
    * comment
    
    * explore
    
    * nested inference example
    
    * found bug in Gamma
    
    * debugging
    
    * port lazyppl monad into monad-bayes
    
    * mh
    
    * add conjugacy tests
    
    * working branch
    
    * tried symbolic
    
    * no symbolic
    
    * remove reflection
    
    * midway: need to finish hmm and enumerator:
    
    * update docs
    
    * build docs manually
    
    * insegel theme
    
    * stanford theme
    
    * lint
    
    * lint
    
    * ormulo
    
    * nix ormolu fixes:
    git push
    
    * docs
    
    * trying to fix mathjax
    
    * remove empirical
    
    * notebooks
    
    * notebooks: sampling
    
    * remove helpers
    
    * models: lts
    
    * models: lts
    
    * weighted sampling
    
    * formatting
    
    * test enumerator
    
    * first attempt to fix normalization bug
    
    * update tests
    
    * working tests: normalNormal goes awry if observations are huge
    
    * cleanup
    
    * Bayesian in class, and remove LinearWithOutliers
    
    * model updates
    
    * yaml
    
    * prepare for PR
    
    * address warnings
    
    * review
    
    * more warnings fixed
    
    * less warnings
    
    * update ghc version stated in cabal
    
    * remove warnings
    
    * fix build
    
    * version bump
    
    * remove safe
    
    * fix merge
    
    * fix rmsmc bug you introduced
    
    * integrator as MonadInfer
    
    * updates
    
    * updates
    
    * updates
    
    * fix the integrator
    
    * inference
    
    * rope in analyticT
    
    * remove integratorT
    
    * nix notebooks
    
    * notebooks
    
    * rmsmc
    
    * hoistFirst bug again
    
    * hoistFirst bug again
    
    * diagrams
    
    * fp notebook
    
    * remove integratorT
    
    * presentation
    
    * notebooks
    
    * cleanup
    
    * docs
    
    * docs
    
    * presentation
    
    * update talk
    
    * notebook
    
    * works, but readd basic
    
    * first working example of marginalization
    
    * renaming
    
    * continue cleanup
    
    * tests: not yet integrated in
    
    * tests: integrated in
    
    * bayesian pmmh
    
    * switch over to new smc
    
    * switch over to new smc
    
    * improve resampleGeneric sum
    
    * independent in Class
    
    * parser
    
    * nbs
    
    * make clean
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * pmmh
    
    * withParticles
    
    * withParticles
    
    * Sequential.Free
    
    * Sequential.Free
    
    * docs
    
    * update nix
    
    * cabal format
    
    * update nix
    
    * fix build
    
    * bump changelog and version. bump ghc version to 9.2.3
    
    * update docs and change mcmc so unweighted is not needed
    
    * remove sequentialCata
    
    * nbs
    
    * merge
    
    * updating notebooks
    
    * mhTrans'
    
    * fix mh
    
    * infinite populations
    
    * simple density
    
    * merge
    
    * lint
    
    * lint
    
    * lazy sampler
    
    * clean up
    
    * Bring back the correct flake
    
    Co-authored-by: Reuben Cohn-Gordon <reubencohn-gordon@Reubens-MacBook-Air.local>
    Co-authored-by: Reuben Cohn-Gordon <reubenharry@gmail.com>
    Co-authored-by: System Administrator <root@Reubens-MacBook-Air.local>

--- flake.nix
+++ flake.nix
@@ -1,59 +1,47 @@
 {
-  description = "JupyterLab Flake";
-
-  inputs = {
-    jupyterWith.url = "github:tweag/jupyterWith";
-    flake-utils.url = "github:numtide/flake-utils";
+  description = "A library for probabilistic programming in Haskell.";
+  nixConfig = {
+    extra-substituters = [
+      "https://tweag-monad-bayes.cachix.org"
+      "https://tweag-wasm.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "tweag-monad-bayes.cachix.org-1:tmmTZ+WvtUMpYWD4LAkfSuNKqSuJyL3N8ZVm/qYtqdc="
+      "tweag-wasm.cachix.org-1:Eu5eBNIJvleiWMEzRBmH3/fzA6a604Umt4lZguKtAU4="
+    ];
   };
-
-  inputs.nixpkgs.url = "nixpkgs/22.05";
-
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
+    haskell-nix-utils.url = "github:TerrorJack/haskell-nix-utils";
+  };
   outputs = {
     self,
     nixpkgs,
-    jupyterWith,
+    flake-compat,
     flake-utils,
+    pre-commit-hooks,
+    haskell-nix-utils,
   }:
-    flake-utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem
+    [
+      # Tier 1 - Tested in CI
+      flake-utils.lib.system.x86_64-linux
+      flake-utils.lib.system.x86_64-darwin
+      # Tier 2 - Not tested in CI (at least for now)
+      flake-utils.lib.system.aarch64-linux
+      flake-utils.lib.system.aarch64-darwin
+    ]
+    (
       system: let
-        myHaskellPackageOverlay = self: super: {
-          myHaskellPackages = super.haskellPackages.override {
-            overrides = hself: hsuper: rec {
-              brick = self.haskell.lib.addBuildDepends (super.haskell.lib.dontHaddock (super.haskell.lib.dontCheck (
-                hself.callCabal2nixWithOptions "brick" (builtins.fetchGit {
-                  url = "https://github.com/jtdaugherty/brick";
-                  rev = "d196695ab7fee3e1b27e2b34ae7d96d369fe1723";
-                }) "" {}
-              ))) [];
-
-              imatrix-sundials = super.haskell.lib.doJailbreak (
-                super.haskell.lib.dontCheck (
-                  hself.callCabal2nix "hmatrix-sundials" (builtins.fetchGit {
-                    url = "https://github.com/novadiscovery/hmatrix-sundials";
-                    rev = "76bfee5b5a8377dc3f7161514761946a60d4834a";
-                  }) {
-                    sundials_arkode = self.sundials;
-                    sundials_cvode = self.sundials;
-                    klu = self.suitesparse;
-                    suitesparseconfig = self.suitesparse;
-                    sundials_sunlinsolklu = self.sundials;
-                    sundials_sunmatrixsparse = self.sundials;
-                  }
-                )
-              );
-            };
-          };
-        };
-
-        pkgs = import nixpkgs {
-          system = system;
-          overlays = [myHaskellPackageOverlay] ++ nixpkgs.lib.attrValues jupyterWith.overlays;
-          config.allowBroken = true;
-        };
-
         inherit (nixpkgs) lib;
-
-        # I don't know why we have this
+        pkgs = nixpkgs.legacyPackages.${system};
+        warnToUpdateNix = pkgs.lib.warn "Consider updating to Nix > 2.7 to remove this warning!";
         src = lib.sourceByRegex self [
           "^benchmark.*$"
           "^models.*$"
@@ -62,60 +50,48 @@
           "^test.*$"
           "^.*\.md"
         ];
-
-        monad-bayes = pkgs.myHaskellPackages.callCabal2nixWithOptions "monad-bayes" src "--benchmark" {};
-
-        iHaskell = pkgs.kernels.iHaskellWith {
-          # Identifier that will appear on the Jupyter interface.
-          name = "nixpkgs";
-          # Libraries to be available to the kernel.
-          packages = p:
-            with p; [
-              pkgs.myHaskellPackages.imatrix-sundials
-              pkgs.myHaskellPackages.hvega
-              pkgs.myHaskellPackages.lens
-              pkgs.myHaskellPackages.log-domain
-              pkgs.myHaskellPackages.katip
-              pkgs.myHaskellPackages.ihaskell-hvega
-              pkgs.myHaskellPackages.ihaskell-diagrams
-              pkgs.myHaskellPackages.text
-              pkgs.myHaskellPackages.diagrams
-              pkgs.myHaskellPackages.diagrams-cairo
-              pkgs.myHaskellPackages.aeson
-              pkgs.myHaskellPackages.lens
-              pkgs.myHaskellPackages.lens-aeson
-              pkgs.myHaskellPackages.pretty-simple
-              pkgs.myHaskellPackages.monad-loops
-              pkgs.myHaskellPackages.hamilton
-              pkgs.myHaskellPackages.hmatrix
-              pkgs.myHaskellPackages.vector-sized
-              pkgs.myHaskellPackages.linear
-              pkgs.myHaskellPackages.recursion-schemes
-              pkgs.myHaskellPackages.data-fix
-              pkgs.myHaskellPackages.free
-              pkgs.myHaskellPackages.comonad
-              pkgs.myHaskellPackages.adjunctions
-              pkgs.myHaskellPackages.distributive
-              pkgs.myHaskellPackages.vector
-              pkgs.myHaskellPackages.megaparsec
-              pkgs.myHaskellPackages.histogram-fill
-              monad-bayes
-            ];
-          # Optional definition of `haskellPackages` to be used.
-          # Useful for overlaying packages.
-          haskellPackages = pkgs.haskell.packages.ghc902;
+        monad-bayes = pkgs.haskell.packages.ghc902.callCabal2nixWithOptions "monad-bayes" src "--benchmark" {};
+        cabal-docspec = let
+          ce =
+            haskell-nix-utils.packages.${system}.pkgs.callPackage
+            (import "${haskell-nix-utils}/project/cabal-extras.nix") {
+              self = haskell-nix-utils;
+              inherit (haskell-nix-utils.packages.${system}) compiler-nix-name index-state;
+            };
+        in
+          ce.cabal-docspec.components.exes.cabal-docspec;
+        monad-bayes-dev = pkgs.mkShell {
+          inputsFrom = [monad-bayes.env];
+          packages = with pre-commit-hooks.packages.${system}; [
+            alejandra
+            cabal-fmt
+            hlint
+            ormolu
+            cabal-docspec
+          ];
+          shellHook =
+            pre-commit.shellHook
+            + ''
+              echo "=== monad-bayes development shell ==="
+            '';
         };
-        jupyterEnvironment = pkgs.jupyterlabWith {
-          kernels = [iHaskell];
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          inherit src;
+          hooks = {
+            alejandra.enable = true;
+            cabal-fmt.enable = true;
+            hlint.enable = false;
+            ormolu.enable = true;
+          };
         };
       in rec {
-        apps.jupterlab = {
-          type = "app";
-          program = "${jupyterEnvironment}/bin/jupyter-lab";
-        };
-        # Is this a spelling mistake?
-        defaultApp = apps.jupterlab;
-        devShell = jupyterEnvironment.env;
+        packages = {inherit monad-bayes pre-commit;};
+        packages.default = packages.monad-bayes;
+        checks = {inherit monad-bayes pre-commit;};
+        devShells.default = monad-bayes-dev;
+        # Needed for backwards compatibility with Nix versions <2.8
+        defaultPackage = warnToUpdateNix packages.default;
+        devShell = warnToUpdateNix devShells.default;
       }
     );
 }
