c1234523b61a8f835e950fc49393458d72a8acc8
alexeusgr <alexeusgr@gmail.com>
2022-09-04 09:27:58 +0300

    add ref to notebooks

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
     pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
     haskell-nix-utils.url = "github:TerrorJack/haskell-nix-utils";
+    jf.url = "git+https://github.com/aleeusgr/monad-bayes?ref=notebooks";
   };
   outputs = {
     self,
