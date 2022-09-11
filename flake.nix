df35f54792f83fcee909581f69d701bd006d43e1
Reuben Cohn-Gordon <reubenharry@gmail.com>
2022-09-10 22:11:42 -0400

    notebooks

--- flake.nix
+++ flake.nix
@@ -1,8 +1,19 @@
 {
-  description = "JupyterLab Flake";
-
+  description = "A library for probabilistic programming in Haskell.";
+  nixConfig = {
+    extra-substituters = [
+      "https://tweag-monad-bayes.cachix.org"
+      "https://tweag-wasm.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "tweag-monad-bayes.cachix.org-1:tmmTZ+WvtUMpYWD4LAkfSuNKqSuJyL3N8ZVm/qYtqdc="
+      "tweag-wasm.cachix.org-1:Eu5eBNIJvleiWMEzRBmH3/fzA6a604Umt4lZguKtAU4="
+    ];
+  };
   inputs = {
-    jupyterWith.url = "github:tweag/jupyterWith";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-compat.flake = false;
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
@@ -10,13 +21,10 @@
     haskell-nix-utils.url = "github:TerrorJack/haskell-nix-utils";
     jupyter-flake.url = "git+https://github.com/tweag/monad-bayes?ref=notebooks";
   };
-
-  inputs.nixpkgs.url = "nixpkgs/22.05";
-
   outputs = {
     self,
     nixpkgs,
-    jupyterWith,
+    flake-compat,
     flake-utils,
     pre-commit-hooks,
     haskell-nix-utils,
@@ -33,44 +41,9 @@
     ]
     (
       system: let
-        myHaskellPackageOverlay = self: super: {
-          myHaskellPackages = super.haskellPackages.override {
-            overrides = hself: hsuper: rec {
-              brick = self.haskell.lib.addBuildDepends (super.haskell.lib.dontHaddock (super.haskell.lib.dontCheck (
-                hself.callCabal2nixWithOptions "brick" (builtins.fetchGit {
-                  url = "https://github.com/jtdaugherty/brick";
-                  rev = "d196695ab7fee3e1b27e2b34ae7d96d369fe1723";
-                }) "" {}
-              ))) [];
-
-              imatrix-sundials = super.haskell.lib.doJailbreak (
-                super.haskell.lib.dontCheck (
-                  hself.callCabal2nix "hmatrix-sundials" (builtins.fetchGit {
-                    url = "https://github.com/novadiscovery/hmatrix-sundials";
-                    rev = "76bfee5b5a8377dc3f7161514761946a60d4834a";
-                  }) {
-                    sundials_arkode = self.sundials;
-                    sundials_cvode = self.sundials;
-                    klu = self.suitesparse;
-                    suitesparseconfig = self.suitesparse;
-                    sundials_sunlinsolklu = self.sundials;
-                    sundials_sunmatrixsparse = self.sundials;
-                  }
-                )
-              );
-            };
-          };
-        };
-
-        pkgs = import nixpkgs {
-          system = system;
-          overlays = [myHaskellPackageOverlay] ++ nixpkgs.lib.attrValues jupyterWith.overlays;
-          config.allowBroken = true;
-        };
-
         inherit (nixpkgs) lib;
-
-        # I don't know why we have this
+        pkgs = nixpkgs.legacyPackages.${system};
+        warnToUpdateNix = pkgs.lib.warn "Consider updating to Nix > 2.7 to remove this warning!";
         src = lib.sourceByRegex self [
           "^benchmark.*$"
           "^models.*$"
@@ -105,17 +78,23 @@
               echo "=== monad-bayes development shell ==="
             '';
         };
-        jupyterEnvironment = pkgs.jupyterlabWith {
-          kernels = [iHaskell];
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          inherit src;
+          hooks = {
+            alejandra.enable = true;
+            cabal-fmt.enable = true;
+            hlint.enable = false;
+            ormolu.enable = true;
+          };
         };
       in rec {
-        apps.jupterlab = {
-          type = "app";
-          program = "${jupyterEnvironment}/bin/jupyter-lab";
-        };
-        # Is this a spelling mistake?
-        defaultApp = apps.jupterlab;
-        devShell = jupyterEnvironment.env;
+        packages = {inherit monad-bayes pre-commit;};
+        packages.default = packages.monad-bayes;
+        checks = {inherit monad-bayes pre-commit;};
+        devShells.default = monad-bayes-dev;
+        # Needed for backwards compatibility with Nix versions <2.8
+        defaultPackage = warnToUpdateNix packages.default;
+        devShell = warnToUpdateNix devShells.default;
       }
     );
 }
