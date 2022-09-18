391aa4d603f80c219da8bbdeb2d620ed874254a3
alexeusgr <alexeusgr@gmail.com>
2022-09-18 09:11:59 +0300

    Seq -> SeqT

---
+++ src/Control/Monad/Bayes/SequentialT.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+
+-- |
+-- Module      : Control.Monad.Bayes.SequentialT
+-- Description : Suspendable probabilistic computation
+-- Copyright   : (c) Adam Scibior, 2015-2020
+-- License     : MIT
+-- Maintainer  : leonhard.markert@tweag.io
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- 'SequentialT' represents a computation that can be suspended.
+module Control.Monad.Bayes.SequentialT
+  ( SequentialT,
+    suspend,
+    finish,
+    advance,
+    finished,
+    hoistFirst,
+    hoist,
+    sequentially,
+    sis,
+  )
+where
+
+import Control.Monad.Bayes.Class
+  ( MonadCond (..),
+    MonadInfer,
+    MonadSample (bernoulli, categorical, random),
+  )
+import Control.Monad.Coroutine
+  ( Coroutine (..),
+    bounce,
+    mapMonad,
+    pogoStick,
+  )
+import Control.Monad.Coroutine.SuspensionFunctors
+  ( Await (..),
+    await,
+  )
+import Control.Monad.Trans (MonadIO, MonadTrans (..))
+import Data.Either (isRight)
+
+-- | Represents a computation that can be suspended at certain points.
+-- The intermediate monadic effects can be extracted, which is particularly
+-- useful for implementation of SequentialT Monte Carlo related methods.
+-- All the probabilistic effects are lifted from the transformed monad, but
+-- also `suspend` is inserted after each `factor`.
+newtype SequentialT m a = SequentialT {runSequentialT :: Coroutine (Await ()) m a}
+  deriving newtype (Functor, Applicative, Monad, MonadTrans, MonadIO)
+
+extract :: Await () a -> a
+extract (Await f) = f ()
+
+instance MonadSample m => MonadSample (SequentialT m) where
+  random = lift random
+  bernoulli = lift . bernoulli
+  categorical = lift . categorical
+
+-- | Execution is 'suspend'ed after each 'score'.
+instance MonadCond m => MonadCond (SequentialT m) where
+  score w = lift (score w) >> suspend
+
+instance MonadInfer m => MonadInfer (SequentialT m)
+
+-- | A point where the computation is paused.
+suspend :: Monad m => SequentialT m ()
+suspend = SequentialT await
+
+-- | Remove the remaining suspension points.
+finish :: Monad m => SequentialT m a -> m a
+finish = pogoStick extract . runSequentialT
+
+-- | Execute to the next suspension point.
+-- If the computation is finished, do nothing.
+--
+-- > finish = finish . advance
+advance :: Monad m => SequentialT m a -> SequentialT m a
+advance = SequentialT . bounce extract . runSequentialT
+
+-- | Return True if no more suspension points remain.
+finished :: Monad m => SequentialT m a -> m Bool
+finished = fmap isRight . resume . runSequentialT
+
+-- | Transform the inner monad.
+-- This operation only applies to computation up to the first suspension.
+hoistFirst :: (forall x. m x -> m x) -> SequentialT m a -> SequentialT m a
+hoistFirst f = SequentialT . Coroutine . f . resume . runSequentialT
+
+-- | Transform the inner monad.
+-- The transformation is applied recursively through all the suspension points.
+hoist ::
+  (Monad m, Monad n) =>
+  (forall x. m x -> n x) ->
+  SequentialT m a ->
+  SequentialT n a
+hoist f = SequentialT . mapMonad f . runSequentialT
+
+-- | Apply a function a given number of times.
+composeCopies :: Int -> (a -> a) -> (a -> a)
+composeCopies k f = foldr (.) id (replicate k f)
+
+-- | SequentialT importance sampling.
+-- Applies a given transformation after each time step.
+sequentially,
+  sis ::
+    Monad m =>
+    -- | transformation
+    (forall x. m x -> m x) ->
+    -- | number of time steps
+    Int ->
+    SequentialT m a ->
+    m a
+sequentially f k = finish . composeCopies k (advance . hoistFirst f)
+
+-- | synonym
+sis = sequentially
---
+++ test/TestSequentialT.hs
@@ -0,0 +1,52 @@
+module TestSequentialT (twoSync, finishedTwoSync, checkTwoSync, checkPreserve, pFinished, isFinished, checkSync) where
+
+import Control.Monad.Bayes.Class
+  ( MonadInfer,
+    MonadSample (uniformD),
+    factor,
+  )
+import Control.Monad.Bayes.Enumerator as Dist (enumerator, mass)
+import Control.Monad.Bayes.SequentialT (advance, finish, finished)
+import Data.AEq (AEq ((~==)))
+import Sprinkler (soft)
+
+twoSync :: MonadInfer m => m Int
+twoSync = do
+  x <- uniformD [0, 1]
+  factor (fromIntegral x)
+  y <- uniformD [0, 1]
+  factor (fromIntegral y)
+  return (x + y)
+
+finishedTwoSync :: MonadInfer m => Int -> m Bool
+finishedTwoSync n = finished (run n twoSync)
+  where
+    run 0 d = d
+    run k d = run (k - 1) (advance d)
+
+checkTwoSync :: Int -> Bool
+checkTwoSync 0 = mass (finishedTwoSync 0) False ~== 1
+checkTwoSync 1 = mass (finishedTwoSync 1) False ~== 1
+checkTwoSync 2 = mass (finishedTwoSync 2) True ~== 1
+checkTwoSync _ = error "Unexpected argument"
+
+sprinkler :: MonadInfer m => m Bool
+sprinkler = Sprinkler.soft
+
+checkPreserve :: Bool
+checkPreserve = enumerator (finish sprinkler) ~== enumerator sprinkler
+
+pFinished :: Int -> Double
+pFinished 0 = 0.8267716535433071
+pFinished 1 = 0.9988062077198566
+pFinished 2 = 1
+pFinished _ = error "Unexpected argument"
+
+isFinished :: MonadInfer m => Int -> m Bool
+isFinished n = finished (run n sprinkler)
+  where
+    run 0 d = d
+    run k d = run (k - 1) (advance d)
+
+checkSync :: Int -> Bool
+checkSync n = mass (isFinished n) True ~== pFinished n
