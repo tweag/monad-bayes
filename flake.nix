259bf28acede18a7eb33ec0302034cc6ed23fee8
mknorps <maria.knorps@tweag.io>
2022-10-07 14:51:47 +0200

    Take flakes from the `notebooks` branch

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
     ];
   };
   inputs = {
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "nixpkgs/22.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
     flake-utils.url = "github:numtide/flake-utils";
@@ -20,16 +19,16 @@
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
     pre-commit-hooks.inputs.flake-utils.follows = "flake-utils";
     haskell-nix-utils.url = "github:TerrorJack/haskell-nix-utils";
-    jupyterWith.url = "github:tweag/jupyterWith";
+    jupyter-flake.url = "git+https://github.com/tweag/monad-bayes?ref=notebooks";
   };
   outputs = {
     self,
     nixpkgs,
-    jupyterWith,
     flake-compat,
     flake-utils,
     pre-commit-hooks,
     haskell-nix-utils,
+    jupyter-flake,
   } @ inputs:
     flake-utils.lib.eachSystem
     [
@@ -43,14 +42,7 @@
     (
       system: let
         inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {
-          system = system;
-          overlays =
-            # [myHaskellPackageOverlay] ++
-            nixpkgs.lib.attrValues jupyterWith.overlays;
-          config.allowBroken = true;
-        };
-        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${system};
         warnToUpdateNix = pkgs.lib.warn "Consider updating to Nix > 2.7 to remove this warning!";
         src = lib.sourceByRegex self [
           "^benchmark.*$"
@@ -61,64 +53,6 @@
           "^.*\.md"
         ];
         monad-bayes = pkgs.haskell.packages.ghc902.callCabal2nixWithOptions "monad-bayes" src "--benchmark" {};
-
-        iHaskell = pkgs.kernels.iHaskellWith {
-          # Identifier that will appear on the Jupyter interface.
-          name = "nixpkgs";
-          # Libraries to be available to the kernel.
-          packages = p:
-            with p; [
-              # pkgs.myHaskellPackages.imatrix-sundials
-              pkgs.haskellPackages.hvega
-              pkgs.haskellPackages.lens
-              pkgs.haskellPackages.log-domain
-              pkgs.haskellPackages.katip
-              pkgs.haskellPackages.ihaskell-hvega
-              pkgs.haskellPackages.ihaskell-diagrams
-              pkgs.haskellPackages.text
-              pkgs.haskellPackages.diagrams
-              pkgs.haskellPackages.diagrams-cairo
-              pkgs.haskellPackages.aeson
-              pkgs.haskellPackages.lens
-              pkgs.haskellPackages.lens-aeson
-              pkgs.haskellPackages.pretty-simple
-              pkgs.haskellPackages.monad-loops
-              pkgs.haskellPackages.hamilton
-              pkgs.haskellPackages.hmatrix
-              pkgs.haskellPackages.vector-sized
-              pkgs.haskellPackages.linear
-              pkgs.haskellPackages.recursion-schemes
-              pkgs.haskellPackages.data-fix
-              pkgs.haskellPackages.free
-              pkgs.haskellPackages.comonad
-              pkgs.haskellPackages.adjunctions
-              pkgs.haskellPackages.distributive
-              pkgs.haskellPackages.vector
-              pkgs.haskellPackages.megaparsec
-              pkgs.haskellPackages.histogram-fill
-              monad-bayes
-            ];
-          # Optional definition of `haskellPackages` to be used.
-          # Useful for overlaying packages.
-          haskellPackages = pkgs.haskell.packages.ghc902;
-        };
-
-        jupyterEnvironment = pkgs.jupyterlabWith {
-          kernels = [iHaskell];
-        };
-
-        jupyterShell = rec {
-          apps.jupterlab = {
-            type = "app";
-            program = "${jupyterEnvironment}/bin/jupyter-lab";
-          };
-          defaultApp = apps.jupyterlab;
-          devShell = jupyterEnvironment.env;
-        };
-
-        # But what do we do with this?
-        # jupyterShell = inputs.jupyter-flake.devShell.${system};
-
         cabal-docspec = let
           ce =
             haskell-nix-utils.packages.${system}.pkgs.callPackage
@@ -128,7 +62,7 @@
             };
         in
           ce.cabal-docspec.components.exes.cabal-docspec;
-
+        jupyterShell = inputs.jupyter-flake.devShell.${system};
         monad-bayes-dev = pkgs.mkShell {
           inputsFrom = [monad-bayes.env jupyterShell];
           packages = with pre-commit-hooks.packages.${system}; [
@@ -141,7 +75,7 @@
           shellHook =
             pre-commit.shellHook
             + ''
-              echo "=== monad-bayes development shell ${jupyterEnvironment} ==="
+              echo "=== monad-bayes development shell ==="
             '';
         };
         pre-commit = pre-commit-hooks.lib.${system}.run {
