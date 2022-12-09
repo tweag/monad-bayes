fd1111d2bbf88df589c8c1d9542e035873b03be3
Dominic Steinitz <dominic@steinitz.org>
2022-12-09 15:15:07 +0000

    Automatic differentiation

--- test/essence.hs
+++ test/essence.hs
@@ -32,7 +32,6 @@ import qualified System.Random.MWC.Distributions as MWC
 
 import Data.List.NonEmpty as NE (NonEmpty ((:|)), toList)
 
-
 import qualified Data.Random as R
 import System.Random.Stateful (setStdGen, newStdGen)
 
@@ -271,3 +270,40 @@ mhRunNormal= do
   stdGen1 <- newIOGenM g1
   stdGen2 <- newIOGenM g2
   runReaderT (unweighted $ (runReaderT (mh 1000 singleObs) stdGen1)) stdGen2
+
+data Dual = Dual Double Double
+  deriving (Eq, Show)
+
+constD :: Double -> Dual
+constD x = Dual x 0
+
+idD :: Double -> Dual
+idD x = Dual x 1.0
+
+instance Num Dual where
+  fromInteger n             = constD $ fromInteger n
+  (Dual x x') + (Dual y y') = Dual (x + y) (x' + y')
+  (Dual x x') * (Dual y y') = Dual (x * y) (x * y' + y * x')
+  negate (Dual x x')        = Dual (negate x) (negate x')
+  signum _                  = undefined
+  abs _                     = undefined
+
+instance Fractional Dual where
+  fromRational p = constD $ fromRational p
+  recip (Dual x x') = Dual (1.0 / x) (-x' / (x * x))
+
+instance Floating Dual where
+  pi = constD pi
+  exp   (Dual x x') = Dual (exp x)   (x' * exp x)
+  log   (Dual x x') = Dual (log x)   (x' / x)
+  sqrt  (Dual x x') = Dual (sqrt x)  (x' / (2 * sqrt x))
+  sin   (Dual x x') = Dual (sin x)   (x' * cos x)
+  cos   (Dual x x') = Dual (cos x)   (x' * (- sin x))
+  sinh  (Dual x x') = Dual (sinh x)  (x' * cosh x)
+  cosh  (Dual x x') = Dual (cosh x)  (x' * sinh x)
+  asin  (Dual x x') = Dual (asin x)  (x' / sqrt (1 - x*x))
+  acos  (Dual x x') = Dual (acos x)  (x' / (-sqrt (1 - x*x)))
+  atan  (Dual x x') = Dual (atan x)  (x' / (1 + x*x))
+  asinh (Dual x x') = Dual (asinh x) (x' / sqrt (1 + x*x))
+  acosh (Dual x x') = Dual (acosh x) (x' / (sqrt (x*x - 1)))
+  atanh (Dual x x') = Dual (atanh x) (x' / (1 - x*x))
